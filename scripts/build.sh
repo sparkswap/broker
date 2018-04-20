abda69ce57632a0e989e4878b15f8631348180ef
Dan Pazuchowski <danpazuchowski@Dans-iMac.local>
2018-04-20 13:24:17 -0700

    update build scripts to use private repo and update relayer proto

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,14 +4,21 @@ set -e
 
 echo ""
 echo "It's time to build! All resistance is futile."
+echo "WARNING: THIS SCRIPT RUNS IN ALL RELATIVE PATHS"
+echo "WARNING: Make sure this was only ran from NPM"
 echo ""
 
-LND_PROTO_URL=${LND_PROTO_URL:-https://raw.githubusercontent.com/lightningnetwork/lnd/master/lnrpc/rpc.proto}
-RELAYER_PROTO_URL=${RELAYER_PROTO_URL:-https://raw.githubusercontent.com/kinesis-exchange/relayer-proto/master/lib/relayer.proto}
-
 # Downloads the LND proto file
+LND_PROTO_URL=${LND_PROTO_URL:-https://raw.githubusercontent.com/lightningnetwork/lnd/master/lnrpc/rpc.proto}
 curl -o ./proto/lnd-rpc.proto $LND_PROTO_URL
-curl -o ./proto/relayer.proto $RELAYER_PROTO_URL
+
+# Clones the relayer repo and copies its relayer.proto file to the
+# root of the directory
+#
+# This works w/ ssh or user/pass
+#
+git clone git@github.com:kinesis-exchange/relayer-proto.git ./proto/relayer
+cp ./proto/relayer/lib/relayer.proto ./proto
 
 # Prepares the downloaded lnd-rpc proto file (installation steps tell you to remove this line)
 # (this is POSIX compliant as the versions of sed differ between OSes)
