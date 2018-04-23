2e741cdf1f7e092c6fe953d5b3f71fc3ded3afb6
Trey Griffith <trey.griffith@gmail.com>
2018-04-22 20:11:34 -0400

    use updating ui with colors, etc

--- broker-cli/orderbook.js
+++ broker-cli/orderbook.js
@@ -7,6 +7,7 @@
  * @option market - required
  */
 
+require('colors')
 const Broker = require('./broker')
 
 /**
@@ -14,14 +15,61 @@ const Broker = require('./broker')
  *
  * TODO: Use a util like clui to represent columns/rows
  * @param {String} market
+ * @param {Array} asks
+ * @param {Array} bids
  * @returns {Void}
  */
-function createUI (market) {
-  console.log(`MARKET: ${market.toUpperCase()}`)
-  console.log('                                                                       ')
-  console.log('                ASK                |                BID                ')
-  console.log('      price      |      depth      |      price      |      depth      ')
-  console.log('-----------------------------------------------------------------------')
+function createUI (market, asks, bids) {
+  console.clear()
+
+  const ui = []
+
+  ui.push("")
+  ui.push(String(`Market: ${market.toUpperCase()}`).bold.white)
+  ui.push("")
+  ui.push(["                ", String("ASK").underline.gray, String("                |                ").gray, String("BID").underline.gray, "                "].join(""))
+  ui.push(String("      price      |      depth      |      price      |      depth      ").gray)
+  ui.push(String("-----------------------------------------------------------------------").gray)
+
+  let rows = Math.max(asks.length, bids.length)
+
+  if(rows === 0) {
+    ui.push(String("                             NO OPEN ORDERS                            ").white)
+  }
+
+  let asksAndBids = [asks, bids]
+  let orderColors = ['red', 'green']
+
+  for(var i=0; i<rows; i++) {
+    let row = ["", ""]
+    // TODO: collapse orders at the same price point into a single line
+    asksAndBids.forEach( (orders, index) => {
+
+      if(orders[i]) {
+        // TODO: pull the 8 out of here and make it per-currency configuration
+        // TODO: make display of amounts consistent with inputs (buys, prices, etc)
+        let price = String(` ${(orders[i].counterAmount / orders[i].baseAmount).toFixed(8)} `)
+        let depth = String(` ${(orders[i].baseAmount * 1e-8).toFixed(8)} `)
+
+        row[index] = [price, depth].map( (field, j) => {
+          while(field.length < 17) {
+            field = ` ${field}`
+          }
+
+          return j ? field.white : field[orderColors[index]]
+        }).join(String('|').gray)
+      } else {
+        row[index] = [Array(17).fill(" ").join(""), Array(17).fill(" ").join("")].join(String('|').gray)
+      }
+
+      while(row[index].length < 17) {
+        row[index] = ` ${row[index]}`
+      }
+    })
+    ui.push(row.join(String('|').gray))
+  }
+
+  console.log(ui.join("\n") + "\n")
 }
 
 async function orderbook (args, opts, logger) {
@@ -39,24 +87,22 @@ async function orderbook (args, opts, logger) {
     // TODO: We should save orders to an internal DB or figure out a way to store
     // this info instead of in memory?
     // (this probably needs to be done in the daemon itself)
-    const orders = []
+    const asks = []
+    const bids = []
 
     // Lets initialize the view AND just to be sure, we will clear the view
-    console.clear()
-    createUI(market)
+    createUI(market, asks, bids)
 
     watchOrder.on('data', (order) => {
       const { orderId, baseAmount, counterAmount, side } = order
 
       if (side === 'ASK') {
-        orders.push({ [orderId]: `      ${baseAmount}      |    ${counterAmount}      |                 |                 ` })
+        asks.push({ counterAmount, baseAmount })
       } else {
-        orders.push({ [orderId]: `                 |                 |      ${baseAmount}      |      ${counterAmount}    ` })
+        bids.push({ counterAmount, baseAmount })
       }
 
-      console.clear()
-      createUI(market)
-      orders.forEach((order) => console.log(Object.values(order)[0]))
+      createUI(market, asks, bids)
     })
 
     watchOrder.on('cancelled', () => logger.info('Stream was cancelled by the server'))
