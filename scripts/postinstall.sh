2d47eb597a8e0a3c928add2e5fbec4b4cf0eb72c
Danny Paz <dan.pazuchowski@gmail.com>
2018-05-01 12:32:05 -0700

    FIX - NPM Install and build (#15)
    * fix commands for npm install
    
    * update documentation

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo ""
-echo "It's time to build! All resistance is futile."
+echo "It's time to BUILD! All resistance is futile."
 echo ""
 
 # Download the relayer proto
@@ -11,7 +11,6 @@ rm -rf ./proto/relayer
 git clone git@github.com:kinesis-exchange/relayer-proto.git ./proto/relayer
 cp ./proto/relayer/lib/relayer.proto ./proto/
 
-# Rest of the installation process
-npm i
+# Rebuild gRPC for docker target
 npm rebuild grpc --target_arch=x64 --target_platform=linux --target_libc=glibc
 npm test
