4947c6d7b89d44e69f3e1bd72f1b86b943a3b397
Danny Paz <dannypaz@Dannys-MacBook-Pro.local>
2018-05-01 21:20:01 -0700

    fix validations for caporal

--- broker-cli/buy.js
+++ broker-cli/buy.js
@@ -35,7 +35,6 @@ async function buy (args, opts, logger) {
     const orderResult = await new Broker(rpcAddress).createOrder(request)
     logger.info(orderResult)
   } catch (e) {
-    console.log(Broker)
     logger.error(e.toString())
   }
 };
