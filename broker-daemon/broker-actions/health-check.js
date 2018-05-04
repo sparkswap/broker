b2541e5d4ab66bbcc48a7f68a90b4ebf44fdfc9f
Martine Ehrlich <martineehrlich@Martines-MacBook-Pro.local>
2018-05-04 10:18:01 -0700

    add specs

--- broker-daemon/broker-actions/health-check.js
+++ broker-daemon/broker-actions/health-check.js
@@ -1,17 +1,16 @@
-const { status } = require('grpc')
 const RelayerClient = require('../relayer')
 const LndEngine = require('lnd-engine')
 
 const { LND_HOST, LND_TLS_CERT, LND_MACAROON } = process.env
 
-async function healthCheck(call, cb) {
+async function healthCheck (call, cb) {
   const lndResStatus = await lndStatus()
   const relayerResStatus = await relayerStatus()
 
   cb(null, {lndStatus: lndResStatus, relayerStatus: relayerResStatus})
 }
 
-async function lndStatus() {
+async function lndStatus () {
   try {
     // TODO: Remove these because they are the default options
     const options = {
@@ -20,23 +19,20 @@ async function lndStatus() {
       macaroonPath: LND_MACAROON
     }
     await new LndEngine(LND_HOST, options).getInfo()
-    return STATUS_CODES.OK  
-
+    return STATUS_CODES.OK
   } catch (e) {
     return e.message
   }
-
 }
 
 async function relayerStatus () {
   const relayer = new RelayerClient()
-    try {
-      await relayer.healthCheck({service: 'Health'})
-      return STATUS_CODES.OK
-
-    } catch (e) {
-      return e.message
-    }
+  try {
+    await relayer.healthCheck({service: 'Health'})
+    return STATUS_CODES.OK
+  } catch (e) {
+    return e.message
+  }
 }
 
 const STATUS_CODES = Object.freeze({
