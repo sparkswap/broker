fedc79e112948104ff7d082aa2a76501fa3b7302
Martine Ehrlich <martineehrlich@Martines-MacBook-Pro.local>
2018-05-10 10:13:25 -0400

    fix hostname

--- broker-daemon/broker-actions/health-check.js
+++ broker-daemon/broker-actions/health-check.js
@@ -40,7 +40,7 @@ async function lndStatus () {
 async function relayerStatus () {
   const relayer = new RelayerClient()
   try {
-    await relayer.healthCheck()
+    await relayer.healthCheck({})
     return STATUS_CODES.OK
   } catch (e) {
     return e.toString()
