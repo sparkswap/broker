5dab7407437fb4f59115faae11d62bb643f1427b
Danny Paz <dannypaz@Dannys-MacBook-Pro.local>
2018-05-15 14:54:41 -0700

    added build image for kcli

--- scripts/build.sh
+++ scripts/build.sh
@@ -27,12 +27,16 @@ rm -rf ./docker/lnd
 rm -f ./docker/docker-compose.yml
 rm -f ./docker/LND-README.md
 
+echo "Installing lnd-engine"
+git clone git@github.com:kinesis-exchange/lnd-engine.git ./node_modules/lnd-engine
+
 # Copy all docker files from lnd-engine and add them to our docker folder
 cp -a ./node_modules/lnd-engine/docker/. ./docker
 cp ./node_modules/lnd-engine/docker-compose.yml ./docker/lnd-docker-compose.yml
 
 # Rename the readme file for docker
 mv ./docker/README.md ./docker/LND-README.md
+
 
 echo "Rebuilding all broker related docker containers/services"
 docker-compose down -v
