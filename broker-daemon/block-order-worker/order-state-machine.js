9fc8b285155e019042bb4e63891a577491eb7ec5
Trey Griffith <trey.griffith@gmail.com>
2018-05-24 17:21:16 -0400

    Add jsdoc to OrderStateMachine

--- broker-daemon/block-order-worker/order-state-machine.js
+++ broker-daemon/block-order-worker/order-state-machine.js
@@ -1,10 +1,35 @@
 const StateMachine = require('javascript-state-machine')
-
+/**
+ * @class Finite State Machine for managing order lifecycle
+ */
 const OrderStateMachine = StateMachine.factory({
+  /**
+   * Definition of the transitions and states for the OrderStateMachine
+   * @type {Array}
+   */
   transitions: [
+    /**
+     * create transition: the first transtion, from 'none' (the default state) to 'created'
+     * @type {Object}
+     */
     { name: 'create', from: 'none', to: 'created' },
+    /**
+     * goto transition: go to the named state from any state, used for re-hydrating from disk
+     * @type {Object}
+     */
     { name: 'goto', from: '*', to: (s) => s }
   ],
+  /**
+   * Instantiate the data on the state machine
+   * This function is effectively a constructor for the state machine
+   * So we pass it all the objects we'll need later.
+   *
+   * @param  {sublevel} options.store         Sublevel partition for storing this order in
+   * @param  {Object} options.logger  
+   * @param  {RelayerClient} options.relayer
+   * @param  {Engine} options.engine
+   * @return {Object}                         Data to attach to the state machine
+   */
   data: function ({ store, logger, relayer, engine }) {
     return { store, logger, relayer, engine, payload: {} }
   },
@@ -15,6 +40,12 @@ const OrderStateMachine = StateMachine.factory({
     onLeaveState: function (lifecycle) {
       this.logger.info(`LEAVE: ${lifecycle.from}`)
     },
+
+    /**
+     * Persist the state machine to disk on entering a new state
+     * @param  {Object} lifecycle Lifecycle object passed by javascript-state-machine
+     * @return {void}
+     */
     onEnterState: async function (lifecycle) {
       this.logger.info(`ENTER: ${lifecycle.to}`)
 
@@ -42,6 +73,20 @@ const OrderStateMachine = StateMachine.factory({
     onTransition: function (lifecycle) {
       this.logger.info(`DURING: ${lifecycle.transition} (from ${lifecycle.from} to ${lifecycle.to})`)
     },
+    /**
+     * Create the order on the relayer during transition.
+     * This function gets called before the `create` transition (triggered by a call to `create`)
+     * Actual creation is done in `onBeforeCreate` so that the transition can be cancelled if creation
+     * on the Relayer fails.
+     * 
+     * @param  {Object} lifecycle             Lifecycle object passed by javascript-state-machine
+     * @param  {String} options.side          Side of the market being taken (i.e. BID or ASK)
+     * @param  {String} options.baseSymbol    Base symbol (e.g. BTC)
+     * @param  {String} options.counterSymbol Counter symbol (e.g. LTC)
+     * @param  {String} options.baseAmount    Amount of base currency (in base units) to be traded
+     * @param  {String} options.counterAmount Amount of counter currency (in base units) to be traded
+     * @return {void}
+     */
     onBeforeCreate: async function (lifecycle, { side, baseSymbol, counterSymbol, baseAmount, counterAmount }) {
       // TODO: move payTo translation somewhere else
       const payTo = `ln:${await this.engine.info.publicKey()}`
@@ -75,6 +120,12 @@ const OrderStateMachine = StateMachine.factory({
   }
 })
 
+/**
+ * Instantiate and create an order
+ * @param  {Object} initParams   Params to pass to the OrderStateMachine constructor (also to the `data` function)
+ * @param  {Object} createParams Params to pass to the create method (also to the `onBeforeCreate` method)
+ * @return {OrderStateMachine}
+ */
 OrderStateMachine.create = async function (initParams, createParams) {
   const osm = new OrderStateMachine(initParams)
   await osm.create(createParams)
@@ -82,6 +133,13 @@ OrderStateMachine.create = async function (initParams, createParams) {
   return osm
 }
 
+/**
+ * Re-hydrate an OrderStateMachine from storage
+ * @param  {Object} initParams    Params to pass to the OrderStateMachine constructor (also to the `data` function)
+ * @param  {String} options.key   Stored key (i.e. the Order ID)
+ * @param  {String} options.value Stringified JSON of the Order State Machine object
+ * @return {OrderStateMachine}
+ */
 OrderStateMachine.fromStore = function (initParams, { key, value }) {
   const parsedValue = JSON.parse(value)
 
