60b0eeecd9d4b81c230f128700d20140d0cb5459
Trey Griffith <trey.griffith@gmail.com>
2018-05-24 17:25:31 -0400

    Add jsdoc to BlockOrder

--- broker-daemon/models/block-order.js
+++ broker-daemon/models/block-order.js
@@ -1,6 +1,19 @@
 const bigInt = require('big-integer')
 
+/**
+ * @class Model representing Block Orders
+ */
 class BlockOrder {
+  /**
+   * Instantiate a new Block Order
+   * @param  {String} options.id          Unique id for the block order
+   * @param  {String} options.marketName  Market name (e.g. BTC/LTC)
+   * @param  {String} options.side        Side of the market being taken (i.e. BID or ASK)
+   * @param  {String} options.amount      Size of the order in base currency (e.g. '10000')
+   * @param  {String} options.price       Limit price for the order (e.g. '100.1')
+   * @param  {String} options.timeInForce Time restriction on the order (e.g. GTC, FOK)
+   * @return {BlockOrder}
+   */
   constructor ({ id, marketName, side, amount, price, timeInForce }) {
     this.id = id
     this.marketName = marketName
@@ -10,10 +23,18 @@ class BlockOrder {
     this.timeInForce = timeInForce
   }
 
+  /**
+   * get key for storage in leveldb
+   * @return {String} Block order id
+   */
   get key () {
     return this.id
   }
 
+  /**
+   * get value for storage in leveldb
+   * @return {String} Stringified JSON object
+   */
   get value () {
     const { marketName, side, amount, price, timeInForce } = this
 
@@ -26,6 +47,12 @@ class BlockOrder {
     })
   }
 
+  /**
+   * Re-instantiate a previously saved BlockOrder
+   * @param  {String} key   Key used to retrieve the BlockOrder
+   * @param  {String} value Value returned from leveldb
+   * @return {BlockOrder}   BlockOrder instance
+   */
   static fromStorage (key, value) {
     const { marketName, side, amount, price, timeInForce } = JSON.parse(value)
     const id = key
