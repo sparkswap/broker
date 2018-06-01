d280a33f19fbac2b57e8e4c24d5974b544fa6b20
Trey Griffith <trey.griffith@gmail.com>
2018-06-01 16:30:06 -0400

    add tests for StateMachinePersistence::getAll and ::fromStore

--- broker-daemon/state-machines/plugins/persistence.spec.js
+++ broker-daemon/state-machines/plugins/persistence.spec.js
@@ -1,4 +1,4 @@
-const { expect, sinon } = require('test/test-helper')
+const { expect, sinon, delay } = require('test/test-helper')
 
 const StateMachinePersistence = require('./persistence')
 const StateMachine = require('../state-machine')
@@ -71,6 +71,182 @@ describe('StateMachinePersistence', () => {
 
       expect(blergh).to.have.been.calledOnce()
       expect(blergh).to.have.been.calledOn(machine)
+    })
+  })
+
+  describe('::getAll', () => {
+    let fakeRecords
+    let Machine
+    let state
+    let store
+    let blergh
+    let blerghValue
+
+    beforeEach(() => {
+      blergh = sinon.stub().callsFake(function (blergh) {
+        if (blergh) {
+          this.blergh = blergh
+        }
+
+        return this.blergh
+      })
+
+      Machine = StateMachine.factory({
+        plugins: [
+          new StateMachinePersistence({
+            additionalFields: {
+              blergh
+            }
+          })
+        ],
+        data: function ({ store }) {
+          return { store }
+        },
+        transitions: [
+          { name: 'step', from: 'none', to: 'first' }
+        ]
+      })
+
+      blerghValue = 'hello'
+      state = 'first'
+
+      fakeRecords = [ ['fakeKey', JSON.stringify({
+        blergh: blerghValue,
+        state
+      })] ]
+
+      store = {
+        put: sinon.stub(),
+        get: sinon.stub(),
+        createReadStream: sinon.stub().callsFake(() => {
+          return {
+            on: async function (event, fn) {
+              if (event === 'data') {
+                for (var i = 0; i < fakeRecords.length; i++) {
+                  await delay(5)
+                  fn({ key: fakeRecords[i][0], value: fakeRecords[i][1] })
+                }
+              } else if (event === 'end') {
+                await delay(fakeRecords.length * 5 + 5)
+                fn()
+              }
+            }
+          }
+        })
+      }
+    })
+
+    it('gets all records from the store', async () => {
+      await Machine.getAll({ store })
+
+      expect(store.createReadStream).to.have.been.calledOnce()
+    })
+
+    it('instantiates an Machine for each record', async () => {
+      const machines = await Machine.getAll({ store })
+
+      expect(machines).to.have.lengthOf(1)
+      expect(machines[0]).to.be.instanceOf(Machine)
+    })
+
+    it('moves the Machine to the correct state', async () => {
+      const machines = await Machine.getAll({ store })
+
+      expect(machines[0]).to.have.property('state', state)
+    })
+
+    it('assigns arbitrary data to the state machine', async () => {
+      const machines = await Machine.getAll({ store })
+
+      expect(machines[0]).to.have.property('blergh', blerghValue)
+    })
+
+    it('calls user defined setters', async () => {
+      const machines = await Machine.getAll({ store })
+
+      expect(blergh).to.have.been.calledOnce()
+      expect(blergh).to.have.been.calledOn(machines[0])
+      expect(blergh).to.have.been.calledWith('hello')
+    })
+  })
+
+  describe('::fromStore', () => {
+    let Machine
+    let key
+    let store
+    let blergh
+    let valueObject
+    let value
+
+    beforeEach(() => {
+      blergh = sinon.stub().callsFake(function (blergh) {
+        if (blergh) {
+          this.blergh = blergh
+        }
+
+        return this.blergh
+      })
+
+      Machine = StateMachine.factory({
+        plugins: [
+          new StateMachinePersistence({
+            additionalFields: {
+              blergh
+            }
+          })
+        ],
+        data: function ({ store }) {
+          return { store }
+        },
+        transitions: [
+          { name: 'step', from: 'none', to: 'first' }
+        ]
+      })
+
+      store = {
+        put: sinon.stub()
+      }
+
+      key = 'fakeKey'
+      valueObject = {
+        state: 'none',
+        blergh: 'hello'
+      }
+      value = JSON.stringify(valueObject)
+    })
+
+    it('initializes a state machine', async () => {
+      const machine = await Machine.fromStore({ store }, { key, value })
+
+      expect(machine).to.be.instanceOf(Machine)
+    })
+
+    it('moves to the default state', async () => {
+      const machine = await Machine.fromStore({ store }, { key, value })
+
+      expect(machine.state).to.be.equal('none')
+    })
+
+    it('moves to the stored state', async () => {
+      valueObject.state = 'first'
+      value = JSON.stringify(valueObject)
+      const machine = await Machine.fromStore({ store }, { key, value })
+
+      expect(machine.state).to.be.equal('first')
+    })
+
+    it('applies arbitrary saved data', async () => {
+      const machine = await Machine.fromStore({ store }, { key, value })
+
+      expect(machine.blergh).to.be.equal(valueObject.blergh)
+    })
+
+    it('calls user defined setters', async () => {
+      const machine = await Machine.fromStore({ store }, { key, value })
+
+      expect(blergh).to.have.been.calledOnce()
+      expect(blergh).to.have.been.calledOn(machine)
+      expect(blergh).to.have.been.calledWith('hello')
     })
   })
 })
