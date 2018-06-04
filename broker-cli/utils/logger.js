66e2372c3b9e330946e3079fcc02a87f430fa18b
Danny Paz <dannypaz@Dannys-MacBook-Pro.local>
2018-06-04 14:51:06 -0700

    added flair to logger

--- broker-cli/utils/logger.js
+++ broker-cli/utils/logger.js
@@ -14,7 +14,20 @@ const logger = winston.createLogger({
   transports: [
     new winston.transports.Console({
       format: winston.format.combine(
-        winston.format.simple()
+        winston.format.timestamp({
+          format: 'YYYY-MM-DD HH:mm:ss'
+        }),
+        winston.format.printf((info) => {
+          if (info.level === 'info') {
+            return `[KCLI - ${info.timestamp}]: `.green + `${info.message}`
+          } else if (info.level === 'error') {
+            return `[KCLI - ${info.timestamp}] [ERROR]: `.red + `${info.message}`
+          } else if (info.level === 'debug') {
+            return `[KCLI - ${info.timestamp}] [DEBUG]: `.yellow + `${info.message}`
+          }
+
+          return `[KCLI - ${info.timestamp}]: ${info.message}`
+        })
       )
     })
   ]
