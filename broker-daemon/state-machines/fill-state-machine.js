34f6b4937183f60aa84e14263285ebd4b39a702c
Trey Griffith <trey.griffith@gmail.com>
2018-06-06 15:58:32 -0700

    skeleton of fill state machine

---
+++ broker-daemon/state-machines/fill-state-machine.js
@@ -0,0 +1,137 @@
+const StateMachine = require('./state-machine')
+const StateMachineHistory = require('javascript-state-machine/lib/history')
+const StateMachinePersistence = require('./plugins/persistence')
+const StateMachineRejection = require('./plugins/rejection')
+const { Fill } = require('../models')
+
+/**
+ * @class Finite State Machine for managing order lifecycle
+ */
+const FillStateMachine = StateMachine.factory({
+  plugins: [
+    new StateMachineHistory(),
+    new StateMachineRejection(),
+    new StateMachinePersistence({
+      /**
+       * @type {StateMachinePersistence~KeyAccessor}
+       * @param {String}   key Unique key for the stored state machine
+       * @returns {String}     Unique key for the state machine
+       */
+      key: function (key) {
+        // this only defines a getter - it will be set by the `order` setter
+        if (!key) {
+          return this.order.key
+        }
+      },
+      additionalFields: {
+        /**
+         * @type {StateMachinePersistence~FieldAccessor}
+         * @param {Object}   fillObject Stored plain object description of the Fill associated with the State machine
+         * @param {String}   key         Unique key for the order/state machine
+         * @returns {Object}             Plain object description of the Fill associated with the State machine
+         */
+        fill: function (fillObject, key) {
+          if (fillObject) {
+            this.fill = Fill.fromObject(key, fillObject)
+          }
+
+          return this.fill.valueObject
+        },
+        /**
+         * @type  {StateMachinePersistence~FieldAccessor}
+         * @param {Array<String>}   history Stored history of states for this state machine
+         * @returns {Array<String>}         History of states for this state machine
+         */
+        history: function (history) {
+          if (history) {
+            this.clearHistory()
+            this.history = history
+          }
+
+          return this.history
+        },
+        /**
+         * @type {StateMachinePersistence~FieldAccessor}
+         * @param {String}   errorMessage Stored error message for a state machine in an errored state
+         * @returns {String}              Error message for a state machine in an errored state
+         */
+        error: function (errorMessage) {
+          if (errorMessage) {
+            this.error = new Error(errorMessage)
+          }
+
+          if (this.error) {
+            return this.error.message
+          }
+        }
+      }
+    })
+  ],
+  /**
+   * Definition of the transitions and states for the FillStateMachine
+   * @type {Array}
+   */
+  transitions: [],
+  /**
+   * Instantiate the data on the state machine
+   * This function is effectively a constructor for the state machine
+   * So we pass it all the objects we'll need later.
+   *
+   * @param  {sublevel}      options.store       Sublevel partition for storing this order in
+   * @param  {Object}        options.logger
+   * @param  {RelayerClient} options.relayer
+   * @param  {Engine}        options.engine
+   * @param  {Function}      options.onRejection A function to handle rejections of the order
+   * @return {Object}                            Data to attach to the state machine
+   */
+  data: function ({ store, logger, relayer, engine, onRejection = function () {} }) {
+    return { store, logger, relayer, engine, onRejection, order: {} }
+  },
+  methods: {
+    onBeforeTransition: function (lifecycle) {
+      this.logger.info(`BEFORE: ${lifecycle.transition}`)
+    },
+    onLeaveState: function (lifecycle) {
+      this.logger.info(`LEAVE: ${lifecycle.from}`)
+    },
+
+    /**
+     * Persist the state machine to disk on entering a new state
+     * @param  {Object} lifecycle Lifecycle object passed by javascript-state-machine
+     * @return {void}
+     */
+    onEnterState: async function (lifecycle) {
+      this.logger.info(`ENTER: ${lifecycle.to}`)
+    },
+    onAfterTransition: function (lifecycle) {
+      this.logger.info(`AFTER: ${lifecycle.transition}`)
+    },
+    onTransition: function (lifecycle) {
+      this.logger.info(`DURING: ${lifecycle.transition} (from ${lifecycle.from} to ${lifecycle.to})`)
+    },
+
+    /**
+     * Handle rejected state by calling a passed in handler
+     * @param  {Object} lifecycle Lifecycle object passed by javascript-state-machine
+     * @return {void}
+     */
+    onAfterReject: async function (lifecycle) {
+      this.onRejection(this.error)
+    }
+  }
+})
+
+/**
+ * Instantiate and create a fill
+ * @param  {Object} initParams   Params to pass to the FillStateMachine constructor (also to the `data` function)
+ * @param  {Object} createParams Params to pass to the create method (also to the `onBeforeCreate` method)
+ * @return {Promise<FillStateMachine>}
+ */
+FillStateMachine.create = async function (initParams, createParams) {
+  const fsm = new FillStateMachine(initParams)
+  await fsm.tryTo('create', createParams)
+
+  return fsm
+}
+
+module.exports = FillStateMachine
