d2c964c224afda27a30a1a86d4d2fca6f4c08371
Trey Griffith <trey.griffith@gmail.com>
2018-06-11 10:50:26 -0700

    add todo comments for refactoring migrateStore into an iterator

--- broker-daemon/utils/migrate-store.js
+++ broker-daemon/utils/migrate-store.js
@@ -1,5 +1,6 @@
 /**
  * Pipe data from one sublevel store into another
+ * @todo Refactor into an iterator pattern using streams2
  * @param  {sublevel} sourceStore       Sublevel that is the source of the data to pipe
  * @param  {sublevel} targetStore       Sublevel that is the destination of the data
  * @param  {Function} createDbOperation Function that returns a sublevel batch-compatible database operation to take on the target store
