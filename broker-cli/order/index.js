87b46729bc6d7b03bef9c1a59d17087be9a95be0
Trey Griffith <trey.griffith@gmail.com>
2018-06-11 18:02:45 -0700

    FEATURE: Add a `order cancel` command to the CLI (#90)
    * add cancel block order command
    
    * add kcli order cancel test

--- broker-cli/order/index.js
+++ broker-cli/order/index.js
@@ -12,10 +12,12 @@ const { validations } = require('../utils')
  * @default
  */
 const SUPPORTED_COMMANDS = Object.freeze({
-  STATUS: 'status'
+  STATUS: 'status',
+  CANCEL: 'cancel'
 })
 
 const status = require('./status')
+const cancel = require('./cancel')
 
 module.exports = (program) => {
   program
@@ -27,15 +29,25 @@ module.exports = (program) => {
     .action(async (args, opts, logger) => {
       const { command, subArguments } = args
 
+      let blockOrderId
+
       switch (command) {
         case SUPPORTED_COMMANDS.STATUS:
-          const [blockOrderId] = subArguments
+          [blockOrderId] = subArguments
 
           args.blockOrderId = validations.isBlockOrderId(blockOrderId || '')
 
           return status(args, opts, logger)
+        case SUPPORTED_COMMANDS.CANCEL:
+          [blockOrderId] = subArguments
+
+          args.blockOrderId = validations.isBlockOrderId(blockOrderId || '')
+
+          return cancel(args, opts, logger)
       }
     })
-    .command(`order ${SUPPORTED_COMMANDS.STATUS}`, status.description)
+    .command(`order ${SUPPORTED_COMMANDS.STATUS}`, 'Get the status of a block order')
     .argument('<blockOrderId>', 'Block order to get status of.', validations.isBlockOrderId)
+    .command(`order ${SUPPORTED_COMMANDS.CANCEL}`, 'Cancel a block order')
+    .argument('<blockOrderId>', 'Block Order to cancel.', validations.isBlockOrderId)
 }
