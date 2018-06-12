91309a6fbe5e2197fef03c2d51eb5206130ef23f
Trey Griffith <trey.griffith@gmail.com>
2018-06-11 17:47:47 -0700

    add a cancel block order service

---
+++ broker-daemon/order-service/cancel-block-order.js
@@ -0,0 +1,31 @@
+const { PublicError } = require('grpc-methods')
+const { BlockOrderNotFoundError } = require('../block-order-worker/errors')
+
+/**
+ * Check on the status of a block order
+ *
+ * @param {GrpcUnaryMethod~request} request - request object
+ * @param {Object} request.params - Request parameters from the client
+ * @param {BlockOrderWorker} request.blockOrderWorker
+ * @param {Object} request.logger
+ * @return {Object}
+ */
+async function cancelBlockOrder ({ params, logger, blockOrderWorker }) {
+  const {
+    blockOrderId
+  } = params
+
+  try {
+    await blockOrderWorker.cancelBlockOrder(blockOrderId)
+
+    return {}
+  } catch (err) {
+    if (err instanceof BlockOrderNotFoundError) {
+      throw new PublicError(err.message, err)
+    }
+
+    throw err
+  }
+}
+
+module.exports = cancelBlockOrder
--- broker-daemon/order-service/index.spec.js
+++ broker-daemon/order-service/index.spec.js
@@ -6,6 +6,7 @@ const OrderService = rewire(path.resolve(__dirname))
 describe('OrderService', () => {
   let createBlockOrderStub
   let getBlockOrderStub
+  let cancelBlockOrderStub
 
   let GrpcMethod
   let register
@@ -52,6 +53,9 @@ describe('OrderService', () => {
 
     getBlockOrderStub = sinon.stub()
     OrderService.__set__('getBlockOrder', getBlockOrderStub)
+
+    cancelBlockOrderStub = sinon.stub()
+    OrderService.__set__('cancelBlockOrder', cancelBlockOrderStub)
   })
 
   beforeEach(() => {
@@ -182,6 +186,44 @@ describe('OrderService', () => {
     it('passes in the response', () => {
       expect(callArgs[3]).to.be.an('object')
       expect(callArgs[3]).to.have.property('GetBlockOrderResponse', proto.GetBlockOrderResponse)
+    })
+  })
+
+  describe('#cancelBlockOrder', () => {
+    let callOrder = 2
+    let callArgs
+
+    beforeEach(() => {
+      callArgs = GrpcMethod.args[callOrder]
+    })
+
+    it('exposes an implementation', () => {
+      expect(server.implementation).to.have.property('cancelBlockOrder')
+      expect(server.implementation.cancelBlockOrder).to.be.a('function')
+    })
+
+    it('creates a GrpcMethod', () => {
+      expect(GrpcMethod).to.have.been.called()
+      expect(GrpcMethod).to.have.been.calledWithNew()
+      expect(server.implementation.cancelBlockOrder).to.be.equal(fakeRegistered)
+    })
+
+    it('provides the method', () => {
+      expect(callArgs[0]).to.be.equal(cancelBlockOrderStub)
+    })
+
+    it('provides a message id', () => {
+      expect(callArgs[1]).to.be.equal('[OrderService:cancelBlockOrder]')
+    })
+
+    describe('request options', () => {
+      it('passes in the logger', () => {
+        expect(callArgs[2]).to.have.property('logger', logger)
+      })
+
+      it('block order worker', () => {
+        expect(callArgs[2]).to.have.property('blockOrderWorker', blockOrderWorker)
+      })
     })
   })
 })
