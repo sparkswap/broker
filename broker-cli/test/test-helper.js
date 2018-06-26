daff89301a25dbd75454b036b686813d0b5b2a20
Trey Griffith <trey.griffith@gmail.com>
2018-06-25 23:09:29 -0400

    add local test and package file for broker-cli

---
+++ broker-cli/test/test-helper.js
@@ -0,0 +1,40 @@
+/**
+ * Kinesis test helper
+ *
+ * NOTE: This file is specifically loaded before all tests so that we
+ * can globally require some files.
+ *
+ */
+
+// unhandled promise rejections are not handled by default in mocha currently,
+// but are deprecated in node.js, so should indicate test failure
+// @see {@link https://github.com/mochajs/mocha/issues/2640|mochajs/mocha#2640}
+process.on('unhandledRejection', () => { throw new Error('Unhandled rejection during testing') })
+
+const sinon = require('sinon')
+const chai = require('chai')
+const sinonChai = require('sinon-chai')
+const dirtyChai = require('dirty-chai')
+const chaiAsPromised = require('chai-as-promised')
+const rewire = require('rewire')
+const delay = require('timeout-as-promise')
+const timekeeper = require('timekeeper')
+
+chai.use(sinonChai)
+chai.use(dirtyChai)
+chai.use(chaiAsPromised)
+
+let sandbox = sinon.createSandbox()
+
+afterEach(function () {
+  sandbox.restore()
+})
+
+module.exports = {
+  chai,
+  expect: chai.expect,
+  sinon: sandbox,
+  rewire,
+  delay,
+  timekeeper
+}
