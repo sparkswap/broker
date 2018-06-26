e54bdef7324eaf5ae195fef8c683b9a28cca72f9
Trey Griffith <trey.griffith@gmail.com>
2018-06-26 11:12:22 -0400

    add script to publish broker-cli to github

---
+++ scripts/publish-broker-cli.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -e
+
+echo ""
+echo "Building kcli for distribution."
+echo ""
+
+
+cd $(dirname "$0")
+cd "../../"
+
+echo "Removing any existing working directory"
+rm -rf ./broker-cli-temp
+
+echo "Creating working directory 'broker-cli-temp' in $(pwd)"
+mkdir broker-cli-temp
+cd broker-cli-temp
+
+echo "Cloning master branch of existing repo at git@github.com:kinesis-exchange/broker.git"
+git clone -b master git@github.com:kinesis-exchange/broker.git .
+
+echo "Latest commit: $(git log --oneline -n 1)"
+
+echo "Pruning git repo to only broker-cli"
+git filter-branch --prune-empty --subdirectory-filter broker-cli master
+
+echo "Updating remote origin"
+
+git remote rm origin
+git remote add origin git@github.com:kinesis-exchange/broker-cli.git
+
+echo "Pushing to GitHub"
+git push -u origin master
+
+echo "Removing working directory"
+
+cd ..
+rm -rf ./broker-cli-temp
+
+echo "Done."
