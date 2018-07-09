65cb2a3113a6730eb19f2c7fd62793b59402a25a
Danny Paz <dan.pazuchowski@gmail.com>
2018-07-09 16:03:05 -0400

    FEATURE - Add LTC to broker (#142)
    * docker-compose changes and build changes
    
    * update pm2 to watch proto and lnd-engine
    
    * add notes about btcd/ltcd
    
    * add timeout as promise fix while we wait for ltc integration w/ swaps
    
    * added kbd image and added build to repo
    
    * removed unused commands and updated readme
    
    * remove log transport
    
    * removed ENGINE_TYPE and text edit on README
    
    * add documentation about ltc using btcd for ln d
    
    * added link for lnd code
    
    * removed bad comments
    
    * added version to lnd-engine
    
    * removed bad comment

--- scripts/add-simnet-cert.local.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-CERT_STRING=$(cd ../relayer && docker-compose exec relayer bash -c 'cat /shared/rpc.cert')
-
-docker-compose exec kbd bash -c "echo \"$CERT_STRING\" >> /shared/simnet-rpc.cert"
-
-docker-compose up -d --force-recreate
