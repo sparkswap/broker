bd609c97601364b615b96eaf0d8bada328d870b9
Danny Paz <dannypaz@Dannys-MacBook-Pro.local>
2018-07-10 10:18:33 -0700

    edit build script to define a new private key

--- scripts/build-images.sh
+++ scripts/build-images.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 
+##################################################################
+#
+# Builds a broker image from scratch
+#
+# NOTE: This functionality requires the use of an ssh key that has read access to the broker
+#       and lnd-engine repositories.
+#       * see https://help.github.com/articles/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent/ for ssh key creation
+#       * see https://blog.github.com/2015-06-16-read-only-deploy-keys/ for read-only deploy key (single repo)
+#
+##################################################################
+
 set -e -u
+
+GITHUB_KEY_PATH=${GITHUB_PRIVATE_KEY_PATH:-~/.ssh/id_rsa}
 
 # NOTE: The names specified with `-t` directly map to the service names in
 # the applicable services docker-compose file
