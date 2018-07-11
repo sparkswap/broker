17da6850a353baaad5fbbe22688eb721f87e83b3
Danny Paz <dannypaz@Dannys-MacBook-Pro.local>
2018-07-11 15:09:02 -0700

    fix funding broker scripts

--- scripts/fund-simnet-wallet.local.sh
+++ scripts/fund-simnet-wallet.local.sh
@@ -11,19 +11,27 @@
 #       included in the lnd_btc container. If this flag was removed, we would need to
 #       create a wallet w/ pass and nmemonic
 #
+# Params:
+#   - SYMBOL (optional, defaults to BTC)
+#   - RELAYER_DIR (optional, defaults to `../relayer`)
+#
 ##########################################
 
 set -e -u
 
-# Hit the kcli endpoint to generate a new wallet address
-echo "Generating new deposit address through KCLI"
+SYMBOL=${SYMBOL:-BTC}
+RELAYER_DIR=${RELAYER_DIR:-../relayer}
 
-WALLET_ADDR=$(./broker-cli/bin/kcli wallet new-deposit-address)
+# TODO: differentiate between lnd and other engines
+echo "Grabbing engine public key from broker"
+
+CONFIG=$(docker-compose exec -T kbd bash -c './broker-cli/bin/kcli config')
+DESTINATION_PUB_KEY=$(node ./scripts/parse-broker-response.js pubkey $CONFIG)
 
 # Restart the btcd container w/ the mining-address for our account
 echo "Running funding script on the relayer w/ wallet addr"
 
-(cd ../relayer && WALLET_ADDR="$WALLET_ADDR" bash ./scripts/fund-simnet-wallet.sh)
+(cd $RELAYER_DIR && ADDR="$DESTINATION_PUB_KEY" SYMBOL=BTC bash ./scripts/fund-simnet-wallet.sh)
 
 echo "Waiting 10 seconds for blocks to be confirmed"
 sleep 10
---
+++ scripts/parse-broker-response.js
@@ -0,0 +1,99 @@
+const [ command, ...data ] = process.argv.slice(2)
+
+const DAEMON_PUBLIC_KEY_DELIMITER = 'daemonPublicKey:'
+const DAEMON_LND_HOST_DELIMITER = 'daemonLndHost:'
+const DAEMON_CHANNEL_POINT_DELIMITER = 'chanPoint:'
+
+/**
+ * Given an array of data from a kcli command, parses the response
+ * to find a specified key
+ *
+ * @param {String} key
+ * @param {Array<String>} data
+ * @return {String} result
+ * @return {Void} nothing is found
+ */
+function getKey (key, data) {
+  let found = false
+
+  for (let k of data) {
+    if (found) return String(k).trim()
+    if (k === key) found = true
+  }
+}
+
+function getKeys (key, data) {
+  const result = []
+  let found = false
+
+  for (let k of data) {
+    if (found) {
+      result.push(String(k).trim())
+      found = false
+      continue
+    }
+
+    if (k === key) found = true
+  }
+
+  return result
+}
+
+/**
+ * Returns a public key from a kcli payload
+ *
+ * @param {Array<string>} data
+ * @return {String} daemonPublicKey
+ */
+function getPublicKey (data) {
+  return getKey(DAEMON_PUBLIC_KEY_DELIMITER, data)
+}
+
+/**
+ * Returns an lnd host ip from a kcli payload
+ *
+ * @param {Array<string>} data
+ * @return {String} daemonLndHost
+ */
+function getHost (data) {
+  return getKey(DAEMON_LND_HOST_DELIMITER, data)
+}
+
+function getChannelPoints (data) {
+  return getKeys(DAEMON_CHANNEL_POINT_DELIMITER, data)
+}
+
+function getChannelPointByPubKey (pubKey, data) {
+  let next = false
+
+  // We skip the first element because that should be the public key
+  for (let i = 1; i < data.length; i++) {
+    if (next && data[i].includes(pubKey)) return String(data[i + 2]).trim()
+    if (data[i] === 'remotePubkey:') next = true
+  }
+}
+
+switch (command.toLowerCase()) {
+  case 'pubkey':
+    const publicKey = getPublicKey(data)
+    if (!publicKey) throw new Error('parser-broker-response.js - No public key found')
+    process.stdout.write(publicKey)
+    break
+  case 'host':
+    const host = getHost(data)
+    if (!host) throw new Error('parser-broker-response.js - No host found')
+    process.stdout.write(host)
+    break
+  case 'channels':
+    let chanPoints = getChannelPoints(data)
+    if (!chanPoints) throw new Error('parser-broker-response.js - No host found')
+    chanPoints = chanPoints.map(r => r.replace('\',', '').replace('\'', ''))
+    chanPoints = chanPoints.join(',')
+    process.stdout.write(chanPoints)
+    break
+  case 'channelpoint':
+    const pubKey = data[0]
+    let chanPoint = getChannelPointByPubKey(pubKey, data)
+    chanPoint = chanPoint.replace('\',', '').replace('\'', '')
+    process.stdout.write(chanPoint)
+}
