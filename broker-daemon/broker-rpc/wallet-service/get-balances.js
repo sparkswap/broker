65a64b8d65b669a2cf3bbc4d88b2451594afd2b9
Danny Paz <dannypaz@Dannys-MacBook-Pro.local>
2018-07-13 11:11:20 -0700

    updated broker proto file for wallet balances, edits to get balances and edits to wallet command for cli

--- broker-daemon/broker-rpc/wallet-service/get-balances.js
+++ broker-daemon/broker-rpc/wallet-service/get-balances.js
@@ -1,30 +1,43 @@
 /**
+ * Given engine information from the daemon, grab the balance from a specified engine
+ *
+ * @param {Array<symbol, engine>} Kinesis Engine
+ * @return {Object} res
+ * @return {Object} res.symbol
+ * @return {Object} res.totalBalance
+ * @return {Object} res.totalChannelBalance
+ */
+async function balancesFromEngine ([symbol, engine]) {
+  const [totalBalance, totalChannelBalance] = await Promise.all([
+    engine.getTotalBalance(),
+    engine.getTotalChannelBalance()
+  ])
+
+  return {
+    symbol,
+    totalBalance,
+    totalChannelBalance
+  }
+}
+
+/**
  * Grabs the daemons lnd wallet balance
  *
  * @function
  * @param {GrpcUnaryMethod~request} request - request object
- * @param {RelayerClient} request.engine
+ * @param {Map} request.engine
  * @param {Object} responses
  * @param {function} responses.GetBalanceResponse
  * @return {responses.GetBalanaceResponse}
  */
-async function getBalances ({ logger, engine }, { GetBalancesResponse }) {
-  const [
-    totalBalance,
-    channelBalances
-  ] = await Promise.all([
-    engine.getTotalBalance(),
-    engine.getChannelBalances()
-  ])
+async function getBalances ({ logger, engines }, { GetBalancesResponse }) {
+  logger.info(`Checking wallet balances for ${engines.size} engines`)
 
-  logger.info(`Received wallet balance: ${totalBalance}`)
+  // We convert the engines map to an array and run totalBalance commands
+  // against each configuration
+  const engineBalances = await Promise.all(Array.from(engines).map(balancesFromEngine))
 
-  const committedBalances = channelBalances.map(({ symbol, value }) => ({ symbol, value: value.toString() }))
-
-  return new GetBalancesResponse({
-    totalBalance,
-    committedBalances
-  })
+  return new GetBalancesResponse({ balances: engineBalances })
 }
 
 module.exports = getBalances
