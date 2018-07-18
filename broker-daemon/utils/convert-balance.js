42df2a2b407518d7489d9093fa170de89708e4fa
Danny Paz <dan.pazuchowski@gmail.com>
2018-07-18 11:45:39 -0700

    FIX - Change convert balance conversion to 60 (#178)
    * change convert balance conversion to 60
    
    * add info about lnd fixed rate
    
    * remove math floor for bad conversion
    
    * fix conversion and tests

--- broker-daemon/utils/convert-balance.js
+++ broker-daemon/utils/convert-balance.js
@@ -1,29 +1,42 @@
 const { Big } = require('./big')
 
-// This is temporarily hardcoded until we have enough data to use our own interal exchange rates
-// TODO fix this conversion once we figure out the problem with LTC max channel balance
+/**
+ * @constant
+ * @type {Object}
+ * @default
+ */
 const MARKET_CONVERSION = {
-  'BTC/LTC': 1
+  // This is a fixed rate/constraint that is hardcoded in LND
+  // see: https://github.com/lightningnetwork/lnd/blob/b0288d46773ac6d45e5dc4d5e6a80dd3034d0b9f/chainregistry.go#L51
+  'BTC/LTC': 60
 }
 
 /**
+ * Given a balance and currency conversion symbols, we return a converted balance.
+ *
+ * NOTE: We round all balances down to avoid returning a balance too large for
+ *       channel opening.
+ *
  * @function
- * @param {String} balance to convert
- * @param {String} currency the currency that the balance is in
- * @param {String} currencyToConvertTo the currency to convert to
- * @return {String} balance in the currencyToConvertTo
+ * @param {String} balance - int64 balance in the `currency` smallest unit e.g. satoshis
+ * @param {String} currency - currency symbol
+ * @param {String} currencyToConvertTo - symbol to convert to
+ * @return {String} int64 string in the `currnecyToConvertTo` smallest unit e.g. litoshis
  */
 function convertBalance (balance, currency, currencyToConvertTo) {
   let multiplier
+
   if (MARKET_CONVERSION.hasOwnProperty(`${currency}/${currencyToConvertTo}`)) {
-    multiplier = MARKET_CONVERSION[`${currency}/${currencyToConvertTo}`]
+    multiplier = Big(MARKET_CONVERSION[`${currency}/${currencyToConvertTo}`])
   } else if (MARKET_CONVERSION.hasOwnProperty(`${currencyToConvertTo}/${currency}`)) {
-    multiplier = 1 / MARKET_CONVERSION[`${currencyToConvertTo}/${currency}`]
+    multiplier = Big(1).div(MARKET_CONVERSION[`${currencyToConvertTo}/${currency}`])
   } else {
     throw Error(`Market ${currency}/${currencyToConvertTo} is not currently supported`)
   }
 
-  return Big(balance).times(multiplier).toString()
+  // We round down to avoid requesting a channel that is too large on the
+  // relayer, however the remainder should only ever be one satoshi
+  return Big(balance).times(multiplier).round(0, 0).toString()
 }
 
 module.exports = convertBalance
