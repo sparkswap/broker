3e4d86b5dc6da71cd362f202dc6a287983017216
Trey Griffith <trey.griffith@gmail.com>
2018-07-18 17:00:16 -0700

    sign data using the public key

--- broker-daemon/relayer/identity.js
+++ broker-daemon/relayer/identity.js
@@ -1,17 +1,54 @@
 const { readFileSync } = require('fs')
+const crypto = require('crypto')
 
 class Identity {
+  /**
+   * Create a new identity for a broker to use with the Relayer
+   * @param  {String}  privKeyPath Absolute path to the private key to use for identity
+   * @param  {String}  pubKeyPath  Absolute path to the public key to use for the identity
+   * @return {Identity}
+   */
   constructor (privKeyPath, pubKeyPath) {
     this.privKeyPath = privKeyPath
     this.pubKeyPath = pubKeyPath
   }
 
+  /**
+   * Load the identity (private and public key) from disk
+   * @return {void}
+   */
   loadSync () {
+    if (!this.privKeyPath) {
+      throw new Error('Private Key path is required to load a Relayer Identity')
+    }
+    if (!this.pubKeyPath) {
+      throw new Error('Public Key path is required to load a Relayer Identity')
+    }
     this.privKey = readFileSync(this.privKeyPath)
     this.pubKey = readFileSync(this.pubKeyPath)
   }
+
+  /**
+   * Sign data with this identity's private key
+   * @param  {String} Base64 encoded data to sign
+   * @return {String} Base64 encoded signature of the data
+   */
+  sign (data) {
+    if (!this.privKey) {
+      throw new Error('Cannot create a signature without a private key.')
+    }
+    const sign = crypto.createSign('sha256')
+    sign.update(Buffer.from(data, 'base64'))
+    return sign.sign(this.privKey, 'base64')
+  }
 }
 
+/**
+ * Create and load a new identity for a broker to use with the Relayer
+ * @param  {String}   privKeyPath Absolute path to the private key to use for identity
+ * @param  {String}   pubKeyPath  Absolute path to the public key to use for the identity
+ * @return {Identity}
+ */
 Identity.load = function (privKeyPath, pubKeyPath) {
   const id = new this(privKeyPath, pubKeyPath)
   id.loadSync()
--- broker-daemon/relayer/identity.spec.js
+++ broker-daemon/relayer/identity.spec.js
@@ -3,7 +3,7 @@ const { sinon, rewire, expect } = require('test/test-helper')
 
 const Identity = rewire(path.resolve(__dirname, 'identity'))
 
-describe('Identity', () => {
+describe.only('Identity', () => {
   let readFileSync
   let privKeyPath
   let pubKeyPath
@@ -53,6 +53,34 @@ describe('Identity', () => {
     it('loads the public key from disk', () => {
       expect(readFileSync).to.have.been.calledWith(pubKeyPath)
       expect(identity).to.have.property('pubKey', fakePubKey)
+    })
+  })
+
+  describe('#sign', () => {
+    let identity
+    let privKey
+
+    beforeEach(() => {
+      identity = new Identity()
+      privKey = '-----BEGIN EC PARAMETERS-----' + '\n' +
+                'BggqhkjOPQMBBw==' + '\n' +
+                '-----END EC PARAMETERS-----' + '\n' +
+                '-----BEGIN EC PRIVATE KEY-----' + '\n' +
+                'MHcCAQEEIFtvZnDK9mgU3HugwAAFfWyO3Vk4mcWIi1XEHl6g2ec5oAoGCCqGSM49' + '\n' +
+                'AwEHoUQDQgAEWOrLBCKQBQkiMJaIV5A05HqWFmR2GR5j8B19bxx7Th3/zmm7mZ8l' + '\n' +
+                'NyseTr1YO7BwN7jKEbMe8Agx5LLCd/IP/A==' + '\n' +
+                '-----END EC PRIVATE KEY-----'
+      identity.privKey = Buffer.from(privKey, 'utf8')
+    })
+
+    it('throws if there is no privKey', () => {
+      identity.privKey = undefined
+
+      expect(() => identity.sign('blah')).to.throw('Cannot create a signature without a private key.')
+    })
+
+    it('creates a signature from a string', () => {
+      expect(identity.sign('blah')).to.be.eql('MEUCIAObz04dVAibxmTo67+X9GFlrfNQ+o81G1ZAUW0hlgReAiEA9J3hiPEWcap7INP0WItBOOGljjBn+KFm9tmdttTg/VI=')
     })
   })
 
