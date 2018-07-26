ee0b46efe8155e656c9c7cba70c722837279c6bc
Danny Paz <dannypaz@Dannys-MacBook-Pro.local>
2018-07-26 11:14:14 -0700

    docker and build changes

--- scripts/build.sh
+++ scripts/build.sh
@@ -13,13 +13,4 @@ echo "Checking broker.proto file for issues"
 npm run broker-proto
 
 echo "Downloading relayer proto files"
-rm -rf ./proto/relayer
-git clone git@github.com:kinesis-exchange/relayer-proto.git ./proto/relayer
-cp ./proto/relayer/relayer.proto ./proto/
-rm -rf ./proto/relayer
-
-echo "Installing lnd-engine"
-git clone git@github.com:kinesis-exchange/lnd-engine.git ./node_modules/lnd-engine
-
-# Remove git file or npm will complain
-rm -rf ./node_modules/lnd-engine/.git
+git clone https://github.com/sparkswap/relayer-proto.git ./proto
