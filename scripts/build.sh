a4baf94b238da9c880a3fae27d133ee7df0b0333
Danny Paz <dannypaz@Dannys-MacBook-Pro.local>
2018-07-26 14:45:51 -0700

    refactor of build scripts for sparkswap release

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -e
+################################################
+# Build script for sparkswapd
+#
+# Params:
+# - INCLUDE_DOCKER (optional, defaults to false)
+################################################
+
+set -e -u
+
+ARG=${1:-false}
 
 echo ""
 echo "It's time to BUILD! All resistance is futile."
@@ -13,4 +22,16 @@ echo "Checking broker.proto file for issues"
 npm run broker-proto
 
 echo "Downloading relayer proto files"
+
+# Blow away proto directory and recreate or git-clone will yell at us
+if [ -d ./proto ]; then
+  rm -rf ./proto
+fi
+
 git clone https://github.com/sparkswap/relayer-proto.git ./proto
+
+# If we want to build images with the command then we can use
+if [ "$ARG" != "no-docker" ]; then
+  echo "Building broker docker images"
+  npm run build-images
+fi
--- scripts/ci-test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-git clone git@github.com:sparkswap/lnd-engine.git ./node_modules/lnd-engine
-npm run format
-npm test
