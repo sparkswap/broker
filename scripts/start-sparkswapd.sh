7abfc2962926fcdcc35646982e796c884859bfd0
Danny Paz <dannypaz@Dannys-MacBook-Pro.local>
2018-08-12 13:42:53 -0700

    updated dev file, added build local command and updated build script

--- scripts/start-sparkswapd.sh
+++ scripts/start-sparkswapd.sh
@@ -10,6 +10,6 @@
 set -e
 
 echo "Downloading Relayer cert..."
-curl -o /secure/relayer-root.pem "${RELAYER_CERT_HOST}/cert" || exit 1
+curl --silent -S --output /secure/relayer-root.pem "${RELAYER_CERT_HOST}/cert" || exit 1
 
 ./broker-daemon/bin/sparkswapd
