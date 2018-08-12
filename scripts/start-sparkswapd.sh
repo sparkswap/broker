92dd2da51db318a06b269926db2cf92677482fc5
Danny Paz <dannypaz@Dannys-MacBook-Pro.local>
2018-08-12 13:54:30 -0700

    add logs for startup

--- scripts/start-sparkswapd.sh
+++ scripts/start-sparkswapd.sh
@@ -11,5 +11,7 @@ set -e
 
 echo "Downloading Relayer cert..."
 curl --silent -S --output /secure/relayer-root.pem "${RELAYER_CERT_HOST}/cert" || exit 1
+echo "Relayer cert downloaded successfully"
 
+echo "Starting sparkswapd"
 ./broker-daemon/bin/sparkswapd
