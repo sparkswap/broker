995db891ad79446037af7834486fc29177b87892
Trey Griffith <trey.griffith@gmail.com>
2018-11-05 11:08:03 -0800

    Fix: url encode order id format to make it compatible with the CLI (#273)
    * url encode block order ids
    
    * scale order summary with screen
    
    * add docs to generate id
    
    * add tests for generate id

--- broker-daemon/utils/generate-id.js
+++ broker-daemon/utils/generate-id.js
@@ -1,8 +1,24 @@
 const crypto = require('crypto')
 
+/**
+ * Generate a random ID string
+ * @return {String} 43 random characters in [base64url encoding]{@link https://tools.ietf.org/html/rfc4648}
+ */
 function generateId () {
   const data = crypto.randomBytes(20).toString('hex')
-  return crypto.createHash('sha256').update(data).digest('base64')
+  return urlEncode(crypto.createHash('sha256').update(data).digest('base64'))
+}
+
+/**
+ * Convert a string from Base64 to [Base64url]{@link https://tools.ietf.org/html/rfc4648}
+ * @param  {String} base64Str Base64 encoded String
+ * @return {String}           Base64url encoded string
+ */
+function urlEncode (base64Str) {
+  return base64Str
+    .replace(/=/g, '')
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_')
 }
 
 module.exports = generateId
