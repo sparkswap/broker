2e21e3e4103df5ffde7517af8743e44b0ddd2c13
Trey Griffith <trey.griffith@gmail.com>
2018-11-26 11:27:08 -0800

    add docs and tests for payInvoice

--- broker-daemon/utils/pay-invoice.js
+++ broker-daemon/utils/pay-invoice.js
@@ -1,3 +1,13 @@
+/**
+ * Pay an invoice and create a refund payment request to have it paid back
+ * @param  {Boolean} required       Whether this payment is required
+ * @param  {String}  paymentRequest Payment Request that should be fulfilled and can be understood by the engine
+ * @param  {Engine}  engine         Payment Channel Network engine to use to pay the invoice
+ * @param  {Logger}  logger
+ * @param  {String}  type           Type of invoice, either `fee` or `deposit`. Used for logging.
+ * @param  {String}  publicId       Public ID of the object being paid for, either a `fillId` or `orderId`
+ * @return {String}                 Payment request for a refund of the same amount that was paid
+ */
 async function payInvoice (required, paymentRequest, engine, logger, type, publicId) {
   if (!required) {
     logger.debug(`Skipping payment for ${type} on ${publicId}, not required`)
