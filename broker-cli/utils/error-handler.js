e304e4cb2648f08e4537ded689c0a57c405a5e5a
Trey Griffith <trey.griffith@gmail.com>
2018-12-10 12:23:36 -0800

    handle internal broker errors and provide some better messaging

--- broker-cli/utils/error-handler.js
+++ broker-cli/utils/error-handler.js
@@ -1,3 +1,4 @@
+require('colors')
 /**
  * Takes in an error object and throws a friendly error if the broker daemon is down
  *
@@ -6,7 +7,10 @@
  */
 function handleError (error) {
   if (error.message === '14 UNAVAILABLE: Connect Failed') {
-    return 'Broker Daemon is unavailable, you may want to check if it\'s still up.'
+    return 'Broker Daemon is unavailable'.red + ', you may want to check if it\'s still up.'
+  } else if (error.code === 13) {
+    return `Broker Daemon encountered an Internal Error: ${error.details} `.red +
+      '\nCheck your Broker Daemon logs (`docker-compose logs -f sparkswapd`) for more information.'
   } else {
     return error
   }
