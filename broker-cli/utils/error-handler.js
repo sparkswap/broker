0c6d92fbac53474a7fdfd3fdc396324b1796b3ea
Trey Griffith <trey.griffith@gmail.com>
2018-12-10 20:09:30 -0800

    Feature: handle internal broker errors (#310)
    * handle internal broker errors and provide some better messaging
    
    * add tests for handling internal broker errors
    
    * use actual grpc status codes to filter
    
    * misspelling

--- broker-cli/utils/error-handler.js
+++ broker-cli/utils/error-handler.js
@@ -1,3 +1,6 @@
+require('colors')
+const { status } = require('grpc')
+
 /**
  * Takes in an error object and throws a friendly error if the broker daemon is down
  *
@@ -5,8 +8,11 @@
  * @return {void}
  */
 function handleError (error) {
-  if (error.message === '14 UNAVAILABLE: Connect Failed') {
-    return 'Broker Daemon is unavailable, you may want to check if it\'s still up.'
+  if (error.code === status.UNAVAILABLE) {
+    return 'Broker Daemon is unavailable'.red + ', you may want to check if it\'s still up.'
+  } else if (error.code === status.INTERNAL) {
+    return `Broker Daemon encountered an Internal Error: ${error.details} `.red +
+      '\nCheck your Broker Daemon logs (`docker-compose logs -f sparkswapd`) for more information.'
   } else {
     return error
   }
