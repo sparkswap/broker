83fbf439ba1edc20a2a44779c475a45bdee83155
Trey Griffith <trey.griffith@gmail.com>
2018-12-11 14:24:06 -0800

    add tests for logger

--- broker-daemon/utils/logger.js
+++ broker-daemon/utils/logger.js
@@ -33,42 +33,49 @@ const SENSITIVE_PROP_LIST = Object.freeze([
  */
 const SENSITIVE_REPLACEMENT = '***FILTERED***'
 
-/**
- * Formatter to filter sensitive data from logs.
- */
-const filterSensitive = winston.format((info) => {
-  const updatedInfo = Object.assign({}, info)
+function createLogger () {
+  /**
+   * Formatter to filter sensitive data from logs.
+   */
+  const filterSensitive = winston.format((info) => {
+    const updatedInfo = Object.assign({}, info)
 
-  Object.entries(updatedInfo).forEach(([key, value]) => {
-    if (SENSITIVE_PROP_LIST.includes(key)) {
-      updatedInfo[key] = SENSITIVE_REPLACEMENT
-    }
+    Object.entries(updatedInfo).forEach(([key, value]) => {
+      if (SENSITIVE_PROP_LIST.includes(key)) {
+        updatedInfo[key] = SENSITIVE_REPLACEMENT
+      }
+    })
+
+    return updatedInfo
   })
 
-  return updatedInfo
-})
-
-const logger = winston.createLogger({
-  level: (process.env.NODE_ENV === 'production') ? 'info' : 'debug',
-  format: winston.format.combine(
-    filterSensitive(),
-    winston.format.timestamp(),
-    winston.format.json()
-  ),
-  json: true,
-  humanReadableUnhandledException: true,
-  handleExceptions: true
-})
-
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
+  const logger = winston.createLogger({
+    level: (process.env.NODE_ENV === 'production') ? 'info' : 'debug',
     format: winston.format.combine(
       filterSensitive(),
       winston.format.timestamp(),
-      winston.format.colorize(),
-      winston.format.simple()
-    )
-  }))
+      winston.format.json()
+    ),
+    json: true,
+    humanReadableUnhandledException: true,
+    handleExceptions: true
+  })
+
+  if (process.env.NODE_ENV !== 'production') {
+    logger.add(new winston.transports.Console({
+      format: winston.format.combine(
+        filterSensitive(),
+        winston.format.timestamp(),
+        winston.format.colorize(),
+        winston.format.simple()
+      )
+    }))
+  }
+
+  return logger
 }
+
+const logger = createLogger()
+logger._createLogger = createLogger
 
 module.exports = logger
