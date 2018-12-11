d0dd4e554d6202364f01f0274f653362aac395aa
Trey Griffith <trey.griffith@gmail.com>
2018-12-10 16:50:32 -0800

    use actual grpc status codes to filter

--- broker-cli/utils/error-handler.js
+++ broker-cli/utils/error-handler.js
@@ -1,4 +1,6 @@
 require('colors')
+const { status } = require('grpc')
+
 /**
  * Takes in an error object and throws a friendly error if the broker daemon is down
  *
@@ -6,9 +8,9 @@ require('colors')
  * @return {void}
  */
 function handleError (error) {
-  if (error.message === '14 UNAVAILABLE: Connect Failed') {
+  if (error.code === status.UNAVAILABLE) {
     return 'Broker Daemon is unavailable'.red + ', you may want to check if it\'s still up.'
-  } else if (error.code === 13) {
+  } else if (error.code === status.INTERNAL) {
     return `Broker Daemon encountered an Internal Error: ${error.details} `.red +
       '\nCheck your Broker Daemon logs (`docker-compose logs -f sparkswapd`) for more information.'
   } else {
