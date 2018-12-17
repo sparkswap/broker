f0fec7b279b3875f3e07e5db17d0ca7005663994
Martine Ehrlich <martineehrlich@gmail.com>
2018-12-17 15:29:25 -0800

    copy to a path not a file

--- broker-cli/scripts/install-config.sh
+++ broker-cli/scripts/install-config.sh
@@ -5,8 +5,8 @@ mkdir -p $DIRECTORY
 
 if [ ! -f "$DIRECTORY/sparkswap.js" ]; then
   echo "Copying sparkswap-sample.js to ~/.sparkswap/sparkswap.js"
-  cp -n \"$(dirname $(which sparkswap))/../lib/node_modules/broker-cli/sample-sparkswap.js\" ~/.sparkswap/sparkswap.js
+  cp -n \"$(dirname $(which sparkswap))/../lib/node_modules/broker-cli/sample-sparkswap.js\" ~/.sparkswap/
 else
   echo "Config file already exists, you can override it with your sample file by running:"
-  echo "cp -n \"$(dirname $(which sparkswap))/../lib/node_modules/broker-cli/sample-sparkswap.js\" ~/.sparkswap/sparkswap.js"
+  echo "cp -n \"$(dirname $(which sparkswap))/../lib/node_modules/broker-cli/sample-sparkswap.js\" ~/.sparkswap/"
 fi
