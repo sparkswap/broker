40dfcb82a23f320e9edbcb6e855f7b72b8f6666a
Mohd Asif <hacker.asif@gmail.com>
2018-12-18 11:25:09 -0800

    Updating post install script to generate auto completion methods (#327)

--- broker-cli/scripts/postinstall.sh
+++ broker-cli/scripts/postinstall.sh
@@ -7,3 +7,12 @@ echo ""
 echo "To do this now, run the below (in bash):"
 echo "cp -n \"$(dirname $(which sparkswap))/../lib/node_modules/broker-cli/sample-.sparkswap.js\" ~/.sparkswap.js"
 echo ""
+echo "You can also enable autocompletion of Sparkswap CLI."
+echo ""
+echo "If your favorite shell is bash, run the below command:"
+echo "echo \"source <(sparkswap completion bash)\" >> ~/.bashrc && source ~/.bashrc"
+echo ""1:
+echo "To do it on zsh, run the below command:"
+echo "echo \"source <(sparkswap completion zsh)\" >> ~/.zshrc && source ~/.zshrc"
+
+echo ""
