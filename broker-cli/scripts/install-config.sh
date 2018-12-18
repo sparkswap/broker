f36075bc50fc3cfa9a462fff94ad4daadeb913c4
Martine Ehrlich <martineehrlich@gmail.com>
2018-12-18 10:12:19 -0800

    fix install config script

--- broker-cli/scripts/install-config.sh
+++ broker-cli/scripts/install-config.sh
@@ -4,16 +4,16 @@ echo ""
 echo "Installing sparkswap config file"
 echo ""
 
-echo "Making directory $DIRECTORY"
 DIRECTORY=~/.sparkswap
+echo "Making directory $DIRECTORY"
 mkdir -p $DIRECTORY
 echo ""
 
 if [ ! -f "$DIRECTORY/config.js" ]; then
-  echo "Copying sparkswap-sample.js to ~/.sparkswap/config.js"
-  cp -n \"$(dirname $(which sparkswap))/../lib/node_modules/broker-cli/sample-config.js\" ~/.sparkswap/config.js
+  echo "Copying sample-config.js to ~/.sparkswap/config.js"
+  cp -n $(dirname $(which sparkswap))/../lib/node_modules/broker-cli/sample-config.js ~/.sparkswap/config.js
 else
   echo "Config file already exists, you can override it with your sample file by running:"
-  echo "cp -n \"$(dirname $(which sparkswap))/../lib/node_modules/broker-cli/sample-config.js\" ~/.sparkswap/config.js"
+  echo "cp -n $(dirname $(which sparkswap))/../lib/node_modules/broker-cli/sample-config.js ~/.sparkswap/config.js"
 fi
 echo ""
