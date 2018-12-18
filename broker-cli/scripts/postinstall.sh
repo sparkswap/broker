f345684e3d27fb8d973ef8218d794c7af415774d
Asif Mohd <asif@sparkswap.com>
2018-12-18 11:14:29 -0800

    Asking customer to setup autoloadyc,

--- broker-cli/scripts/postinstall.sh
+++ broker-cli/scripts/postinstall.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-# Setup auto-completion for bash
-if [-f ~/.bashrc]; then
-    echo "source <(sparkswap completion bash)" >> ~/.bashrc && source ~/.bashrc
-fi    
-# Setup auto-completion for zsh
-if [-f ~/.zshrc]; then
-    echo "source <(sparkswap completion zsh)" >> ~/.zshrc && source ~/.zshrc
-fi    
-
 echo "Successfully installed Ϟ SparkSwap Broker CLI (sparkswap)!"
 echo ""
 echo "To set up your custom configuration for the SparkSwap CLI, copy 'sample-.sparkswap.js' to your home directory as '.sparkswap.js'."
 echo ""
 echo "To do this now, run the below (in bash):"
 echo "cp -n \"$(dirname $(which sparkswap))/../lib/node_modules/broker-cli/sample-.sparkswap.js\" ~/.sparkswap.js"
+echo ""
+echo "You can also enable autocompletion of Sparkswap CLI."
+echo ""
+echo "If your favorite shell is bash, run the below command:"
+echo "echo \"source <(sparkswap completion bash)\" >> ~/.bashrc && source ~/.bashrc"
+echo ""1:
+echo "To do it on zsh, run the below command:"
+echo "echo \"source <(sparkswap completion zsh)\" >> ~/.zshrc && source ~/.zshrc"
+
 echo ""
