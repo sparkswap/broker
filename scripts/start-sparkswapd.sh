fbcd65eea04f9ff621b0e49d72bc2c75e36654dc
Martine Ehrlich <martineehrlich@gmail.com>
2018-12-19 13:47:13 -0800

    use encrypt cert instead of downloading cert from relayer for mainnet

--- scripts/start-sparkswapd.sh
+++ scripts/start-sparkswapd.sh
@@ -8,10 +8,14 @@
 ################################################
 
 set -e
-
-echo "Downloading Relayer cert..."
-curl --silent -S --output /secure/relayer-root.pem "${RELAYER_CERT_HOST}/cert" || exit 1
-echo "Relayer cert downloaded successfully"
-
+if [ "$NETWORK" == "mainnet" ]; then
+  echo "Copying mainnet cert"
+  cp /home/app/relayer-root.pem /secure/relayer-root.pem
+  echo "Relayer cert copied successfully"
+else
+  echo "Downloading Relayer cert..."
+  curl --silent -S --output /secure/relayer-root.pem "${RELAYER_CERT_HOST}/cert" || exit 1
+  echo "Relayer cert downloaded successfully"
+fi
 echo "Starting sparkswapd"
 ./broker-daemon/bin/sparkswapd
