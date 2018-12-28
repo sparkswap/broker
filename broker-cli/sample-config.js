0fbccb0309fd91470169980eddd88877eb00e7ab
Trey Griffith <trey.griffith@gmail.com>
2018-12-28 12:51:57 -0500

    update sample config to include default auth config

--- broker-cli/sample-config.js
+++ broker-cli/sample-config.js
@@ -14,30 +14,30 @@ module.exports = {
    * Address of the host for the Broker Daemon gRPC Server
    * @type {String}
    */
-  // rpcAddress: 'localhost:27492',
+  rpcAddress: 'localhost:27492',
 
   /**
    * Default path of the Broker Daemons RPC Public Cert
    * @type {String}
    */
-  // rpcCertPath: '~/.sparkswap/certs/broker-rpc-tls.cert',
+  rpcCertPath: '~/.sparkswap/certs/broker-rpc-tls.cert',
 
   /**
    * Configuration for SSL between the CLI and Daemon. This setting is only required
    * if you will be hosting the daemon remotely
    * @type {Boolean}
    */
-  // disableAuth: false
+  disableAuth: false
 
   /**
    * The username specified on the remote Broker Daemon RPC
    * @type {String}
    */
-  // rpcUser: 'sparkswap',
+  rpcUser: 'sparkswap',
 
   /**
    * The password specified on the remote Broker Daemon RPC
    * @type {String}
    */
-  // rpcPass: 'sparkswap'
+  rpcPass: 'sparkswap'
 }
