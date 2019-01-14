0dba3f3210e791956ebb566d0ae9afb04a1ccdbc
Danny Paz <dannypaz@Dannys-MacBook-Pro.local>
2019-01-14 15:03:42 -0800

    added changes for building standalone docker image for broker

--- scripts/build-images.sh
+++ scripts/build-images.sh
@@ -1,38 +1,11 @@
 #!/usr/bin/env bash
 
 ##################################################################
-#
 # Builds a broker image from scratch
-#
-# Params:
-#   - EXTERNAL_ADDRESS (optional, defaults to localhost)
-#   - RELAYER_PROTO_BRANCH (defaults to master)
-#   - RELAYER_COMMIT_SHA (defaults to the master branch commit sha) (This overrides RELAYER_PROTO_BRANCH)
-#
 ##################################################################
 
 set -e -u
 
-KEY_PATH=${KEY_PATH}
-CERT_PATH=${CERT_PATH}
-RELAYER_PROTO_BRANCH=${RELAYER_PROTO_BRANCH:-master}
-
-# We now grab the commit-shas related to the branches set above. These will
-# be used if the user has no specificed a specific commit-sha to be used in
-# image creation
-#
-CURRENT_RELAYER_PROTO_COMMIT_SHA=`git ls-remote git://github.com/sparkswap/relayer-proto | grep "refs/heads/$RELAYER_PROTO_BRANCH$" | cut -f 1`
-
-# If the user has specified a COMMIT_SHA to be used for image creation, then
-# we will use that, otherwise we will default to the master branch's commit sha
-# OR a user provided branch.
-#
-RELAYER_PROTO_COMMIT_SHA=${RELAYER_COMMIT_SHA:-$CURRENT_RELAYER_PROTO_COMMIT_SHA}
-
 # NOTE: The names specified with `-t` directly map to the service names in
 # the applicable services docker-compose file
-docker build -t sparkswap_sparkswapd -f ./docker/sparkswapd/Dockerfile ./  \
-  --build-arg RELAYER_PROTO_COMMIT_SHA=$RELAYER_PROTO_COMMIT_SHA \
-  --build-arg RELAYER_PROTO_BRANCH=$RELAYER_PROTO_BRANCH \
-  --build-arg KEY_PATH=$KEY_PATH \
-  --build-arg CERT_PATH=$CERT_PATH
+docker build -t sparkswap_sparkswapd -f ./docker/sparkswapd/Dockerfile ./
--- scripts/build.sh
+++ scripts/build.sh
@@ -7,13 +7,14 @@
 # - EXTERNAL_ADDRESS (optional, for hosted daemons)
 ################################################
 
-set -e -u
+set -eu
 
 # Setting this env is ONLY required for a hosted broker setup.
 #
 # This address is used during the build process so that certs can be generated
 # correctly for a hosted (remote) broker daemon.
 EXTERNAL_ADDRESS=${EXTERNAL_ADDRESS:-}
+RELAYER_PROTO_VERSION='v0.2.2-beta'
 
 ARG=${1:-false}
 
@@ -22,41 +23,31 @@ echo "It's time to BUILD! All resistance is futile."
 echo ""
 
 echo "Reinstalling dependencies"
-npm install
+npm install --quiet
 
 echo "Checking broker.proto file for issues"
 npm run broker-proto
 
 echo "Downloading relayer proto files"
-
 # Blow away proto directory and recreate or git-clone will yell at us
 if [ -d ./proto ]; then
   rm -rf ./proto
 fi
 
-git clone https://github.com/sparkswap/relayer-proto.git ./proto
+git clone -b ${RELAYER_PROTO_VERSION} https://github.com/sparkswap/relayer-proto.git ./proto
 rm -rf ./proto/.git
-
-# If the build is local, then we will copy related files for dev usage
-# for a local SimNet network
-if [ "$ARG" == "local" ]; then
-  echo "Copying env simnet local file to .env"
-  cp ./.env-simnet-sample ./.env
-  echo "Copying dev file to 'docker-compose.override.yml'"
-  cp ./docker-compose.simnet.sample.yml ./docker-compose.override.yml
-elif [ -f docker-compose.override.yml ]; then
-  # Let the user know that an override file exists which may mean that the user
-  # will have settings they do not expect
-  echo "A 'docker-compose.override.yml' file exists, but you are not using a supported"
-  echo "environment: local or regtest"
-  echo ""
-  echo "WARNING: This may add unwanted settings to the broker that could effect how your daemon runs."
-  echo ""
-fi
 
 echo "Generating certificates for RPC"
 
-# Generate certs for the CLI
+#############################################
+# Keypair Generation for SSL to the broker
+#
+# This step creates certs to allow a user to host a broker on a remote machine
+# and have connections to their daemon be secured through ssl
+#
+# Primary use is TLS between Broker-CLI and Broker Daemon
+#
+#############################################
 rm -rf ./certs
 mkdir -p ./certs
 
@@ -68,38 +59,57 @@ EXTERNAL_ADDRESS=${EXTERNAL_ADDRESS:-localhost}
 
 openssl ecparam -genkey -name prime256v1 -out $KEY_PATH
 
-openssl req -new -sha256 \
+openssl req -new -sha256 -key $KEY_PATH -out $CSR_PATH \
   -reqexts SAN \
   -extensions SAN \
   -config <(cat /etc/ssl/openssl.cnf \
       <(printf "\n[SAN]\nsubjectAltName=DNS:${EXTERNAL_ADDRESS},DNS:localhost")) \
-  -key $KEY_PATH \
-  -out $CSR_PATH \
   -subj "/CN=$EXTERNAL_ADDRESS/O=sparkswap"
 
-openssl req -x509 -sha256 \
+openssl req -x509 -sha256 -key $KEY_PATH -in $CSR_PATH -out $CERT_PATH -days 36500 \
   -reqexts SAN \
   -extensions SAN \
   -config <(cat /etc/ssl/openssl.cnf \
       <(printf "\n[SAN]\nsubjectAltName=DNS:${EXTERNAL_ADDRESS},DNS:localhost")) \
-  -days 36500 \
-  -key $KEY_PATH \
-  -in $CSR_PATH \
-  -out $CERT_PATH
 
 rm $CSR_PATH
 
+#############################################
+# Keypair Generation for Relayer
+#
+# This step creates certs to allow the broker to authenticate/auth for orders
+# on the relayer
+#
+# We use a "Secure key exchange algorithm" here because these keys are exchanged
+# via a non secure channel. (ECDH)
+#
+#############################################
+
+ID_PRIV_KEY='./certs/broker-identity.private.pem'
+ID_PUB_KEY='./certs/broker-identity.public.pem'
+
+RUN openssl ecparam -name prime256v1 -genkey -noout -out ${ID_PRIV_KEY}
+RUN openssl ec -in ${ID_PRIV_KEY} -pubout -out ${ID_PUB_KEY}
+
 echo "Building broker docker images"
-KEY_PATH=$KEY_PATH CERT_PATH=$CERT_PATH npm run build-images
+if [ "$ARG" == "no-docker" ]; then
+  KEY_PATH=$KEY_PATH CERT_PATH=$CERT_PATH npm run build-images
+fi
+
+if [ -f docker-compose.override.yml ]; then
+  # Let the user know that an override file exists which may mean that the user
+  # will have settings they do not expect
+  echo ""
+  echo "WARNING: A 'docker-compose.override.yml' file exists"
+  echo "WARNING: This may add unwanted settings to the broker that could effect how your daemon runs."
+  echo ""
+fi
 
 # We can skip the copying of certs to a local directory if the current build is
 # a standalone broker
 if [ "$ARG" == "no-cli" ]; then
-  rm -rf ./certs
   exit 0
 fi
-
-echo "Copying certs"
 
 echo "Making local ~/.sparkswap certs directory"
 DIRECTORY=~/.sparkswap
@@ -107,6 +117,3 @@ mkdir -p $DIRECTORY/certs
 
 echo "Copying certs to local certs directory"
 cp $CERT_PATH $DIRECTORY/certs/broker-rpc-tls.cert
-
-# Clean certs directory after use
-rm -rf ./certs
