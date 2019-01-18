64d4cb3029f439fa8ed74ca4335848cba15700cf
Danny Paz <dannypaz@Dannys-MacBook-Pro.local>
2019-01-18 10:50:16 -0800

    change parameters for sparkswapd
    fix tests and add context
    
    remove package lock
    
    added package lock
    
    cleanup of configuration for engine types and comment changes
    
    remove logging of start script from docker logs
    
    descriptions for sparkswap
    
    fix tests for sparkswapd cli
    
    added test for config defaults
    
    fix regtest sample for cert server
    
    added path name to variables and removed tag for disable-auth
    
    separate bitcoind/litecoind into own docker-compose files (#382)
    
    * separate bitcoind/litecoind into own docker-compose files
    
    * clean up comments in env sample files
    
    * set default value in bitcoind litecoind docker-compose files
    
    merge with master and add http proxy flag

--- broker-daemon/bin/sparkswapd.spec.js
+++ broker-daemon/bin/sparkswapd.spec.js
@@ -2,11 +2,8 @@ const path = require('path')
 const { sinon, rewire, expect } = require('test/test-helper')
 
 const sparkswapd = rewire(path.resolve(__dirname, 'sparkswapd.js'))
+const config = rewire(path.resolve(__dirname, '..', 'config.json'))
 
-/**
- * IMPORTANT NOTE: We skip all of the environment variable (default) tests because
- * we bind to process.env before `rewire` has a chance to replace the values.
- */
 describe('sparkswapd', () => {
   let BrokerDaemon
   let argv
@@ -36,162 +33,221 @@ describe('sparkswapd', () => {
     expect(BrokerDaemon.prototype.initialize).to.have.been.calledOnce()
   })
 
-  it('provides the daemon SSL private/public key paths', () => {
-    const rpcKeyPath = {
-      privRpcKeyPath: '/path/to/priv/key',
-      pubRpcKeyPath: '/path/pub/key'
+  it('calls broker daemon with defaults', () => {
+    sparkswapd(argv)
+
+    const {
+      dataDir,
+      interchainRouterAddress,
+      idPubKeyPath: pubIdKeyPath,
+      idPrivKeyPath: privIdKeyPath,
+      markets,
+      disableAuth,
+      rpc: {
+        address: rpcAddress,
+        user: rpcUser,
+        pass: rpcPass,
+        pubKeyPath: pubRpcKeyPath,
+        privKeyPath: privRpcKeyPath
+      },
+      relayer: {
+        rpcHost: relayerRpcHost,
+        certPath: relayerCertPath
+      }
+    } = config
+
+    const brokerOptions = {
+      dataDir,
+      interchainRouterAddress,
+      pubIdKeyPath,
+      privIdKeyPath,
+      marketNames: [markets],
+      disableAuth,
+      rpcAddress,
+      rpcUser,
+      rpcPass,
+      pubRpcKeyPath,
+      privRpcKeyPath,
+      relayerOptions: {
+        relayerRpcHost,
+        relayerCertPath
+      },
+      engines: sinon.match.object
     }
 
-    argv.push('--rpc-privkey-path')
-    argv.push(rpcKeyPath.privRpcKeyPath)
-    argv.push('--rpc-pubkey-path')
-    argv.push(rpcKeyPath.pubRpcKeyPath)
-
-    sparkswapd(argv)
-
-    expect(BrokerDaemon).to.have.been.calledWith(sinon.match(rpcKeyPath))
+    expect(BrokerDaemon).to.have.been.calledWith(sinon.match(brokerOptions))
   })
 
-  it('provides the identity private/public key paths', () => {
-    const privIdKeyPath = '/path/to/priv/key'
-    const pubIdKeyPath = '/path/pub/key'
+  context('broker params', () => {
+    it('provides the identity private/public key paths', () => {
+      const privIdKeyPath = '/path/to/priv/key'
+      const pubIdKeyPath = '/path/pub/key'
 
-    argv.push('--id-privkey-path')
-    argv.push(privIdKeyPath)
-    argv.push('--id-pubkey-path')
-    argv.push(pubIdKeyPath)
+      argv.push('--id-priv-key-path')
+      argv.push(privIdKeyPath)
+      argv.push('--id-pub-key-path')
+      argv.push(pubIdKeyPath)
 
-    sparkswapd(argv)
+      sparkswapd(argv)
 
-    expect(BrokerDaemon).to.have.been.calledWith(sinon.match({ }))
+      expect(BrokerDaemon).to.have.been.calledWith(sinon.match({ }))
+    })
+
+    it('provides an interchain router address', () => {
+      const interchainRouterAddress = '0.0.0.0:9876'
+
+      argv.push('--interchain-router-address')
+      argv.push(interchainRouterAddress)
+
+      sparkswapd(argv)
+
+      expect(BrokerDaemon).to.have.been.calledWith(sinon.match({ interchainRouterAddress }))
+    })
+
+    it('provides an data dir', () => {
+      const dataDir = '/dev/null'
+      argv.push('--data-dir')
+      argv.push(dataDir)
+
+      sparkswapd(argv)
+
+      expect(BrokerDaemon).to.have.been.calledWith(sinon.match({ dataDir }))
+    })
+
+    it('provides the markets', () => {
+      const markets = 'BTC/LTC,ABC/XYZ'
+      argv.push('--markets')
+      argv.push(markets)
+
+      sparkswapd(argv)
+
+      const marketNames = markets.split(',')
+
+      expect(BrokerDaemon).to.have.been.calledWith(sinon.match({ marketNames }))
+    })
   })
 
-  it('provides an rpc user/pass', () => {
-    const rpcUser = 'sparkswap'
-    const rpcPass = 'passwd'
+  context('rpc params', () => {
+    it('provides the daemon SSL private/public key paths', () => {
+      const rpcKeyPath = {
+        privRpcKeyPath: '/path/to/priv/key',
+        pubRpcKeyPath: '/path/pub/key'
+      }
 
-    argv.push('--rpc-user')
-    argv.push(rpcUser)
-    argv.push('--rpc-pass')
-    argv.push(rpcPass)
+      argv.push('--rpc.priv-key-path')
+      argv.push(rpcKeyPath.privRpcKeyPath)
+      argv.push('--rpc.pub-key-path')
+      argv.push(rpcKeyPath.pubRpcKeyPath)
 
-    sparkswapd(argv)
+      sparkswapd(argv)
 
-    expect(BrokerDaemon).to.have.been.calledWith(sinon.match({ rpcUser, rpcPass }))
+      expect(BrokerDaemon).to.have.been.calledWith(sinon.match(rpcKeyPath))
+    })
+
+    it('provides an rpc user/pass', () => {
+      const rpcUser = 'sparkswap'
+      const rpcPass = 'passwd'
+
+      argv.push('--rpc.user')
+      argv.push(rpcUser)
+      argv.push('--rpc.pass')
+      argv.push(rpcPass)
+
+      sparkswapd(argv)
+
+      expect(BrokerDaemon).to.have.been.calledWith(sinon.match({ rpcUser, rpcPass }))
+    })
+
+    it('provides an rpc address', () => {
+      const rpcAddress = '0.0.0.0:9876'
+      argv.push('--rpc.address')
+      argv.push(rpcAddress)
+
+      sparkswapd(argv)
+
+      expect(BrokerDaemon).to.have.been.calledWith(sinon.match({ rpcAddress }))
+    })
   })
 
-  it('provides an rpc address', () => {
-    const rpcAddress = '0.0.0.0:9876'
-    argv.push('--rpc-address')
-    argv.push(rpcAddress)
+  context('relayer options', () => {
+    it('provides a relayer host', () => {
+      const relayerHost = 'example.com:9876'
+      argv.push('--relayer.rpc-host')
+      argv.push(relayerHost)
 
-    sparkswapd(argv)
+      sparkswapd(argv)
 
-    expect(BrokerDaemon).to.have.been.calledWith(sinon.match({ rpcAddress }))
+      expect(BrokerDaemon).to.have.been.calledWith(sinon.match({ relayerOptions: { relayerRpcHost: relayerHost } }))
+    })
+
+    it('provides a relayer cert path', () => {
+      const relayerCertPath = '/path/to/relayer/root.pem'
+      argv.push('--relayer.cert-path')
+      argv.push(relayerCertPath)
+
+      sparkswapd(argv)
+
+      expect(BrokerDaemon).to.have.been.calledWith(sinon.match({ relayerOptions: { relayerCertPath } }))
+    })
   })
 
-  it('provides an interchain router address', () => {
-    const interchainRouterAddress = '0.0.0.0:9876'
-    argv.push('--interchain-router-address')
-    argv.push(interchainRouterAddress)
+  context('engine options', () => {
+    it('provides the engine type', () => {
+      const ltcEngineType = 'LND'
+      argv.push('--ltc.engine-type')
+      argv.push(ltcEngineType)
 
-    sparkswapd(argv)
+      sparkswapd(argv)
 
-    expect(BrokerDaemon).to.have.been.calledWith(sinon.match({ interchainRouterAddress }))
-  })
+      const { engines } = BrokerDaemon.args[0][0]
+      expect(engines.LTC).to.have.property('type', ltcEngineType)
+    })
 
-  it('provides an data dir', () => {
-    const dataDir = '/dev/null'
-    argv.push('--data-dir')
-    argv.push(dataDir)
+    it('provides the engine rpc address', () => {
+      const ltcEngineType = 'LND'
+      const ltcLndRpc = 'localhost:1337'
 
-    sparkswapd(argv)
+      argv.push('--ltc.engine-type')
+      argv.push(ltcEngineType)
+      argv.push('--ltc.rpc-host')
+      argv.push(ltcLndRpc)
 
-    expect(BrokerDaemon).to.have.been.calledWith(sinon.match({ dataDir }))
-  })
+      sparkswapd(argv)
 
-  it('provides a relayer host', () => {
-    const relayerHost = 'example.com:9876'
-    argv.push('--relayer-host')
-    argv.push(relayerHost)
+      const { engines } = BrokerDaemon.args[0][0]
+      expect(engines.LTC).to.have.property('lndRpc', ltcLndRpc)
+    })
 
-    sparkswapd(argv)
+    it('provides the engine tls cert', () => {
+      const ltcEngineType = 'LND'
+      const ltcLndTls = '/dev/null'
 
-    expect(BrokerDaemon).to.have.been.calledWith(sinon.match({ relayerOptions: { relayerRpcHost: relayerHost } }))
-  })
+      argv.push('--ltc.engine-type')
+      argv.push(ltcEngineType)
+      argv.push('--ltc.tls-cert')
+      argv.push(ltcLndTls)
 
-  it('provides a relayer cert path', () => {
-    const relayerCertPath = '/path/to/relayer/root.pem'
-    argv.push('--relayer-cert-path')
-    argv.push(relayerCertPath)
+      sparkswapd(argv)
 
-    sparkswapd(argv)
+      const { engines } = BrokerDaemon.args[0][0]
+      expect(engines.LTC).to.have.property('lndTls', ltcLndTls)
+    })
 
-    expect(BrokerDaemon).to.have.been.calledWith(sinon.match({ relayerOptions: { relayerCertPath } }))
-  })
+    context('LND specific configuration', () => {
+      it('provides the engine macaroon path', () => {
+        const ltcEngineType = 'LND'
+        const ltcLndMacaroon = '/dev/null'
 
-  it('provides the markets', () => {
-    const markets = 'BTC/LTC,ABC/XYZ'
-    argv.push('--markets')
-    argv.push(markets)
+        argv.push('--ltc.engine-type')
+        argv.push(ltcEngineType)
+        argv.push('--ltc.lnd-macaroon')
+        argv.push(ltcLndMacaroon)
 
-    sparkswapd(argv)
+        sparkswapd(argv)
 
-    const marketNames = markets.split(',')
-
-    expect(BrokerDaemon).to.have.been.calledWith(sinon.match({ marketNames }))
-  })
-
-  it('provides the engine type', () => {
-    const ltcEngineType = 'LND'
-    argv.push('--ltc-engine-type')
-    argv.push(ltcEngineType)
-
-    sparkswapd(argv)
-
-    const { engines } = BrokerDaemon.args[0][0]
-    expect(engines.LTC).to.have.property('type', ltcEngineType)
-  })
-
-  it('provides the engine rpc address', () => {
-    const ltcEngineType = 'LND'
-    argv.push('--ltc-engine-type')
-    argv.push(ltcEngineType)
-    const ltcLndRpc = 'localhost:1337'
-    argv.push('--ltc-lnd-rpc')
-    argv.push(ltcLndRpc)
-
-    sparkswapd(argv)
-
-    const { engines } = BrokerDaemon.args[0][0]
-    expect(engines.LTC).to.have.property('lndRpc', ltcLndRpc)
-  })
-
-  it('provides the engine tls cert', () => {
-    const ltcEngineType = 'LND'
-    argv.push('--ltc-engine-type')
-    argv.push(ltcEngineType)
-    const ltcLndTls = '/dev/null'
-    argv.push('--ltc-lnd-tls')
-    argv.push(ltcLndTls)
-
-    sparkswapd(argv)
-
-    const { engines } = BrokerDaemon.args[0][0]
-    expect(engines.LTC).to.have.property('lndTls', ltcLndTls)
-  })
-
-  it('provides the engine macaroon path', () => {
-    const ltcEngineType = 'LND'
-    argv.push('--ltc-engine-type')
-    argv.push(ltcEngineType)
-    const ltcLndMacaroon = '/dev/null'
-    argv.push('--ltc-lnd-macaroon')
-    argv.push(ltcLndMacaroon)
-
-    sparkswapd(argv)
-
-    const { engines } = BrokerDaemon.args[0][0]
-    expect(engines.LTC).to.have.property('lndMacaroon', ltcLndMacaroon)
+        const { engines } = BrokerDaemon.args[0][0]
+        expect(engines.LTC).to.have.property('lndMacaroon', ltcLndMacaroon)
+      })
+    })
   })
 })
