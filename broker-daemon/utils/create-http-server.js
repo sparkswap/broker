09a1f7bb3dcc3fe735d062a1b0932d2f6a1c055d
Danny Paz <dannypaz@Dannys-MacBook-Pro.local>
2019-02-01 10:51:50 -0800

    added handle404 function and added request logging to http servers

--- broker-daemon/utils/create-http-server.js
+++ broker-daemon/utils/create-http-server.js
@@ -11,10 +11,11 @@ const corsMiddleware = require('./enable-cors')
 /**
  * http 404 handler for our http (express) server
  *
- * @param {object} _req - request object
+ * @param {object} req - request object
  * @param {object} res - response object
  */
-function handle404 (_req, res) {
+function handle404 (logger, req, res) {
+  logger.debug('Received request but had no route', { url: req.url })
   res.status(404).send('404')
 }
 
@@ -36,13 +37,19 @@ function createHttpServer (protoPath, rpcAddress, { disableAuth = false, enableC
   app.use(bodyParser.json())
   app.use(bodyParser.urlencoded({ extended: false }))
 
+  // Log all requests for our http server
+  app.use((req, _res, next) => {
+    logger.info(`${req.ip} - broker-http-server "${req.method} ${req.url} ${req.protocol}" ${req.headers['user-agent']}`)
+    next()
+  })
+
   if (enableCors) {
     app.use(corsMiddleware())
   }
 
   // If the RPC address we use for daemon is set to a default route (0.0.0.0)
   // then we want to make sure that we are instead making a request w/ grpc-gateway
-  // to local host since 0.0.0.0 would be an invalid address w/ the current cert
+  // to `localhost`. `0.0.0.0` would be an invalid address w/ the current cert
   // setup
   if (rpcAddress.includes('0.0.0.0')) {
     rpcAddress = rpcAddress.replace('0.0.0.0', 'localhost')
@@ -50,7 +57,7 @@ function createHttpServer (protoPath, rpcAddress, { disableAuth = false, enableC
 
   if (disableAuth) {
     app.use('/', grpcGateway([`/${protoPath}`], rpcAddress))
-    app.use(handle404)
+    app.use(handle404.bind(null, logger))
     return app
   } else {
     const key = fs.readFileSync(privKeyPath)
@@ -60,7 +67,7 @@ function createHttpServer (protoPath, rpcAddress, { disableAuth = false, enableC
     logger.debug(`Securing RPC proxy connections with TLS: key: ${privKeyPath}, cert: ${pubKeyPath}`)
 
     app.use('/', grpcGateway([`/${protoPath}`], rpcAddress, channelCredentials))
-    app.use(handle404)
+    app.use(handle404.bind(null, logger))
     return https.createServer({ key, cert }, app)
   }
 }
