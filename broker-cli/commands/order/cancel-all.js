033768fd53a53093033080e8323b5fd94128b302
Martine Ehrlich <martineehrlich@gmail.com>
2019-03-04 16:52:27 -0800

    add cancel all orders command (#411)
    * add functin for cancelling all active block orders for a given market
    
    * updates to naming and promise.all
    
    * add cancel all orders command
    
    * fix logging for cancelled orders
    
    * fixjsdoc
    
    * add specs;
    
    * fix specs and condition and add logger
    
    * add specs and question prompt
    
    * fix multiline logging
    
    * typo
    
    * add output
    
    * jsdoc

---
+++ broker-cli/commands/order/cancel-all.js
@@ -0,0 +1,52 @@
+const BrokerDaemonClient = require('../../broker-daemon-client')
+const { handleError, askQuestion } = require('../../utils')
+
+/**
+ * @constant
+ * @type {Array<string>}
+ * @default
+ */
+const ACCEPTED_ANSWERS = Object.freeze(['y', 'yes'])
+/**
+ * sparkswap order cancel-all
+ *
+ * ex: `sparkswap order cancel-all --market btc/ltc'
+ *
+ * @param {Object} args
+ * @param {string} args.market
+ * @param {Object} opts
+ * @param {string} opts.rpcAddress
+ * @param {string} opts.market
+ * @param {Logger} logger
+ */
+async function cancelAll (args, opts, logger) {
+  const {
+    rpcAddress,
+    market
+  } = opts
+
+  const request = {
+    market
+  }
+
+  const answer = await askQuestion(`Are you sure you want to cancel all your orders on the ${market} market? (Y/N)`)
+  if (!ACCEPTED_ANSWERS.includes(answer.toLowerCase())) return
+
+  try {
+    const client = new BrokerDaemonClient(rpcAddress)
+    const {
+      cancelledOrders = [],
+      failedToCancelOrders = []
+    } = await client.orderService.cancelAllBlockOrders(request)
+
+    logger.info(`Successfully cancelled ${cancelledOrders.length} orders on ${market} market.`)
+    if (failedToCancelOrders.length) {
+      logger.info(`Unable to cancel ${failedToCancelOrders.length} orders on ${market} market.`)
+      logger.info('Check your Broker Daemon logs (`docker-compose logs -f sparkswapd`) for more information.')
+    }
+  } catch (e) {
+    logger.error(handleError(e))
+  }
+};
+
+module.exports = cancelAll
