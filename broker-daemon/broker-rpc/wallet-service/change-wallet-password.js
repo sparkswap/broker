9c2da35cc4d2986b82e0a208262cd39bd9ad37ce
Danny Paz <danny@sparkswap.com>
2019-05-07 14:14:25 -0700

    added tests for rpc change wallet password

--- broker-daemon/broker-rpc/wallet-service/change-wallet-password.js
+++ broker-daemon/broker-rpc/wallet-service/change-wallet-password.js
@@ -30,7 +30,7 @@ async function changeWalletPassword ({ logger, params, engines }, { EmptyRespons
   if (!engine.isLocked) {
     logger.error(`Engine for ${symbol} is not locked. Current status: ${engine.status}`)
     throw new Error(`Unable to change your wallet password, engine for ${symbol} is currently: ${engine.status}.
-                     Your engine needs to be in status LOCKED`)
+                     Your engine needs to be in a locked status`)
   }
 
   await engine.changeWalletPassword(currentPassword, newPassword)
