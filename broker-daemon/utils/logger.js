a22b87f0ab95dc7f1ad1380d4d1680e36bef6799
Danny Paz <danny@sparkswap.com>
2019-05-08 18:29:44 -0700

    add DEBUG env for log levels (#516)

--- broker-daemon/utils/logger.js
+++ broker-daemon/utils/logger.js
@@ -50,7 +50,7 @@ function createLogger () {
   })
 
   const logger = winston.createLogger({
-    level: (process.env.NODE_ENV === 'production') ? 'info' : 'debug',
+    level: process.env.DEBUG || 'info',
     format: winston.format.combine(
       filterSensitive(),
       winston.format.timestamp(),
