bdd2aaf30c62bf45f34d467ac544e1fe5837713b
Danny Paz <danny@sparkswap.com>
2019-05-10 11:18:32 -0700

    add logging to see time for get-block-orders

--- broker-daemon/broker-rpc/order-service/get-block-orders.js
+++ broker-daemon/broker-rpc/order-service/get-block-orders.js
@@ -11,8 +11,12 @@
  */
 async function getBlockOrders ({ params, logger, blockOrderWorker }, { GetBlockOrdersResponse }) {
   try {
+    logger.info(new Date())
     const orders = await blockOrderWorker.getBlockOrders(params.market)
+    logger.info(new Date())
     const blockOrders = orders.map(order => order.serializeSummary())
+    logger.info(`Block order length: ${blockOrders.length}`)
+
     // The gRPC constructor does not serialize block orders correctly, so we instead
     // return an object.
     return { blockOrders }
