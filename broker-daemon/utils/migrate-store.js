8885fd991d2b875f0e5ca3206e4e7f7c5079b8d5
Danny Paz <danny@sparkswap.com>
2019-06-20 13:23:21 -0700

    changes to batch changing of records

--- broker-daemon/utils/migrate-store.js
+++ broker-daemon/utils/migrate-store.js
@@ -9,35 +9,25 @@
 async function migrateStore (sourceStore, targetStore, createDbOperation) {
   return new Promise((resolve, reject) => {
     const stream = sourceStore.createReadStream()
-    let batch = []
-
-    function flush (done = () => {}) {
-      if (!batch.length) {
-        return process.nextTick(done)
-      }
-
-      targetStore.batch(batch, (err) => {
-        if (err) return reject(err)
-        done()
-      })
-
-      // clear the batch
-      batch = []
-    }
-
-    stream.on('error', reject)
-
-    stream.on('end', () => {
-      flush(resolve)
-    })
+    const batch = targetStore.batch()
 
     stream.on('data', ({ key, value }) => {
       const op = createDbOperation(key, value)
 
       if (op) {
-        batch.push(op)
+        batch.add(op)
       }
     })
+
+    stream.on('end', () => {
+      if (!batch.length) {
+        return resolve()
+      }
+
+      batch.write(() => resolve()).catch(err => reject(err))
+    })
+
+    stream.on('error', reject)
   })
 }
 
