9757bfecd1501da6c2a76b1e65194bed9caf63f9
Danny Paz <danny@sparkswap.com>
2019-06-21 11:25:30 -0700

    add batch in progress for migration of store

--- broker-daemon/utils/migrate-store.js
+++ broker-daemon/utils/migrate-store.js
@@ -4,30 +4,72 @@
  * @param  {sublevel} sourceStore       - Sublevel that is the source of the data to pipe
  * @param  {sublevel} targetStore       - Sublevel that is the destination of the data
  * @param  {Function} createDbOperation - Function that returns a sublevel batch-compatible database operation to take on the target store
+ * @param  {number}   batchSize         - Number of operations to batch before executing on the target database
  * @returns {Promise<void>}
  */
-async function migrateStore (sourceStore, targetStore, createDbOperation) {
+async function migrateStore (sourceStore, targetStore, createDbOperation, batchSize = 2000) {
   return new Promise((resolve, reject) => {
     const stream = sourceStore.createReadStream()
-    const batch = targetStore.batch()
+
+    // `batchInProgress` allows us to batch save records in sequence and lock the operation
+    // to make sure we are not inserting records out of the order they are received.
+    //
+    // We set this value to automatically resolve on the first use. This value is then
+    // subsequently set when a batch is processed
+    let batchInProgress = async () => {}
+    let batchResolve
+
+    let batch = []
+
+    async function flush (done = () => {}) {
+      if (!batch.length) {
+        return process.nextTick(done)
+      }
+
+      await batchInProgress()
+
+      batchInProgress = () => {
+        return new Promise((resolve, reject) => {
+          batchResolve = resolve
+        })
+      }
+
+      try {
+        targetStore.batch(batch, (err) => {
+          if (err) {
+            batchResolve()
+            return reject(err)
+          }
+
+          batchResolve()
+          done()
+        })
+      } catch (e) {
+        batchResolve()
+        throw (e)
+      }
+
+      // clear the batch
+      batch = []
+    }
+
+    stream.on('error', reject)
+
+    stream.on('end', () => {
+      flush(resolve)
+    })
 
     stream.on('data', ({ key, value }) => {
       const op = createDbOperation(key, value)
 
       if (op) {
-        batch.add(op)
-      }
-    })
-
-    stream.on('end', () => {
-      if (!batch.length) {
-        return resolve()
+        batch.push(op)
       }
 
-      batch.write(() => resolve()).catch(err => reject(err))
+      if (batch.length >= batchSize) {
+        flush()
+      }
     })
-
-    stream.on('error', reject)
   })
 }
 
