3125b90fe7a646006153a27aff4bacb29f39162e
Danny Paz <danny@sparkswap.com>
2019-06-28 16:19:51 -0700

    added recover wallet method

---
+++ broker-daemon/broker-rpc/wallet-service/recover-wallet.js
@@ -0,0 +1,50 @@
+const fs = require('fs')
+
+/**
+ * Attempts to recover a wallet for a new lnd instance
+ *
+ * @param {GrpcUnaryMethod~request} request - request object
+ * @param {Logger} request.logger
+ * @param {Object<string>} request.params
+ * @param {string} request.params.symbol - currency symbol of the wallet e.g. `BTC`
+ * @param {string} request.params.password - password for the existing wallet
+ * @param {string} request.params.seed - string representation of 24 word mnemonic seed delimited by spaces
+ * @param {string} request.params.backupPath - backup path file
+ * @param {Map<Engine>} request.engines
+ * @param {Object} responses
+ * @param {Function} responses.EmptyResponse
+ * @returns {EmptyResponse}
+ */
+async function recoverWallet ({ logger, params, engines }, { EmptyResponse }) {
+  const {
+    symbol,
+    password,
+    seed,
+    backupPath
+  } = params
+
+  const engine = engines.get(symbol)
+
+  if (!engine) {
+    logger.error(`Could not find engine: ${symbol}`)
+    throw new Error(`Unable to create wallet for engine: ${symbol}`)
+  }
+
+  let backup
+
+  if (backupPath) {
+    backup = fs.readFileSync(backupPath)
+  }
+
+  let seedList
+
+  if (seed) {
+    seedList = seed.split(' ')
+  }
+
+  await engine.restoreWallet(password, seedList, backup)
+
+  return new EmptyResponse({})
+}
+
+module.exports = recoverWallet
