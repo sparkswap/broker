abe097faca6b2a3dcfbd6cd45ed0942ac20b9b2b
Zain Iqbal Allarakhia <zain.allarakhia@gmail.com>
2019-07-11 12:04:12 -0700

    remove trades from cli

--- broker-cli/commands/market/index.js
+++ broker-cli/commands/market/index.js
@@ -16,12 +16,10 @@ const { RPC_ADDRESS_HELP_STRING, MARKET_NAME_HELP_STRING } = require('../../util
 const SUPPORTED_COMMANDS = Object.freeze({
   SUPPORTED_MARKETS: 'supported-markets',
   MARKET_STATS: 'market-stats',
-  TRADES: 'trades'
 })
 
 const supportedMarkets = require('./supported-markets')
 const marketStats = require('./market-stats')
-const trades = require('./trades')
 
 module.exports = (program) => {
   program
@@ -41,20 +39,12 @@ module.exports = (program) => {
         case SUPPORTED_COMMANDS.MARKET_STATS:
           opts.market = validations.isMarketName(market)
           return marketStats(opts, logger)
-        case SUPPORTED_COMMANDS.TRADES:
-          const [since, limit] = subArguments
-          args.since = since
-          args.limit = limit
-
-          opts.market = validations.isMarketName(market)
-          return trades(args, opts, logger)
       }
     })
     .command(`market ${SUPPORTED_COMMANDS.SUPPORTED_MARKETS}`, 'Get the markets currently supported')
     .option('--rpc-address [rpc-address]', RPC_ADDRESS_HELP_STRING, validations.isHost)
     .command(`market ${SUPPORTED_COMMANDS.MARKET_STATS}`, 'Get statistics (price ticker information) for a particular market for a period of the last 24 hours')
     .option('--market <marketName>', MARKET_NAME_HELP_STRING, validations.isMarketName, null, true)
-    .command(`market ${SUPPORTED_COMMANDS.TRADES}`, 'Get detailed information about trades from a given time range')
     .argument('<since>', 'Start datetime in ISO format e.g. 2018-04-23T10:26:00.996Z', validations.isDate)
     .argument('<limit>', 'Number of records to be retrieved', validations.isPositiveInteger)
     .option('--market <marketName>', MARKET_NAME_HELP_STRING, validations.isMarketName, null, true)
