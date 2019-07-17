a0fcb1c7884eace275fa96176d66b256693fba2c
Chris Clark <chris@sparkswap.com>
2019-07-17 10:16:42 -0700

    Update

--- broker-daemon/broker-rpc/order-service/create-order.js
+++ broker-daemon/broker-rpc/order-service/create-order.js
@@ -4,14 +4,42 @@ const { GrpcResponse, Big } = require('../../utils')
 
 /** @typedef {import('../broker-rpc-server').GrpcUnaryMethodRequest} GrpcUnaryMethodRequest */
 
+/** @typedef {object} Amount
+ * @property {string} symbol
+ * @property {string} value
+ */
+
+/** @typedef {object} OrderRequest
+ * @property {Amount} sourceAmount
+ * @property {string} sourceAddress
+ * @property {Amount} destinationAmount
+ * @property {string} destinationAddress
+ * @property {string} roleRestriction
+ */
+
+/**
+ * Normalize numeric string
+ * @param {string} value
+ * @returns {string}
+ */
 function normalizeValue (value) {
   return (new Big(value)).toString()
 }
 
+/**
+ * Normalize numeric strings in an amount
+ * @param {Amount} amount
+ * @returns {Amount}
+ */
 function normalizeAmount (amount) {
   return Object.assign({}, amount, { value: normalizeValue(amount.value) })
 }
 
+/**
+ * Normalize numeric strings in an order
+ * @param {OrderRequest} order
+ * @returns {OrderRequest}
+ */
 function normalizeOrder (order) {
   return Object.assign({}, order, {
     sourceAmount: normalizeAmount(order.sourceAmount),
